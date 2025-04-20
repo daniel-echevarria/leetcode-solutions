b669d856b8e583c41f91684c16fe561baf7fe2ba
Kasper Bak <dan.echevarri@gmail.com>
2025-04-20 18:41:37 +0200

    Add problem 228

---
+++ Javascript/228-summary-ranges.js
@@ -0,0 +1,9 @@
+var summaryRanges = function (nums) {};
+
+console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
+
+// Algo
+// declare a variable result with initial value of ''
+// declare a variable previous with an initial value of nums[0] + ->
+// iterate through nums from index 1
+// if the current num is equal to previous + 1
