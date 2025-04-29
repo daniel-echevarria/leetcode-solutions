e91058000a11fc14daecb0655f39430cf8cb3546
Kasper Bak <dan.echevarri@gmail.com>
2025-04-29 11:56:50 +0200

    Add problem 138

---
+++ Javascript/138-copy-list-with-random-pointer.js
@@ -0,0 +1,17 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+var copyRandomList = function (head) {};
+
+// Algo copyRandomList
+// Given the head of a list
+// Declare a variable current that gets the head
+// Declare a variable newHead that gets a new Node
+// newHead val gets head val
+// newHead next gets a new node
