448f92df979feb0d3ad8ef6a01579b6c7e284c41
Kasper Bak <dan.echevarri@gmail.com>
2025-05-05 21:11:12 +0200

    Add solution to problem 19
    19. Remove Nth Node From End of List

--- Javascript/19-remove-node-from-end.js
+++ Javascript/19-remove-node-from-end.js
@@ -1,1 +1,19 @@
-var removeNthFromEnd = function (head, n) {};
+var removeNthFromEnd = function (head, n) {
+  const dummy = new ListNode(0, head);
+  const nodes = [dummy];
+  while (head) {
+    nodes.push(head);
+    head = head.next;
+  }
+  if (nodes.length < 2) return head;
+  const badNodeIndex = nodes.length - n;
+  nodes[badNodeIndex - 1].next = nodes[badNodeIndex + 1] || null;
+  return dummy.next;
+};
+
+// Algo
+// Traverse the list, push all the nodes to a nodes array
+// Create a dummyNode that points to head
+// Declare a variable badNodeIndex that gets nodes.length - n
+// select the node before badNode and make it point to the node just after
+// return dummy.next
