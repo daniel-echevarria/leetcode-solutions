8eb20e02b40cebfc9a1822c10aa40e6c6e5c735a
Kasper Bak <dan.echevarri@gmail.com>
2025-05-08 12:18:28 +0200

    Add optimal solution

--- Javascript/82-remove-duplicates.js
+++ Javascript/82-remove-duplicates.js
@@ -1,4 +1,4 @@
-var deleteDuplicates = function (head) {
+var deleteDuplicates2 = function (head) {
   const met = [];
   const dup = [];
   const dummy = new ListNode(0, head);
@@ -28,3 +28,49 @@ var deleteDuplicates = function (head) {
 // after traversing the list once
 // traverse the list by checking if the next value is in duplicates
 // if it is nextValue gets next next  and keep going until next is null
+
+const deleteDuplicates = (head) => {
+  const dummy = new ListNode(0, head);
+  let currentValue = null;
+  let prev = dummy;
+  while (head) {
+    if (head.val === currentValue) {
+      prev.next = head.next;
+      head = head.next;
+      continue;
+    }
+    if (!head.next) {
+      return dummy.next;
+    }
+    currentValue = head.val;
+    if (head.next.val === currentValue) {
+      prev.next = head.next.next;
+      head = prev.next;
+      continue;
+    }
+    prev = head;
+    head = head.next;
+  }
+  return dummy.next;
+};
+
+[1, 1, 1];
+
+[1, 2, 3, 3, 4, 4, 5];
+
+[1, 1, 1, 2, 3];
+
+// Algo one pass
+// declare a dummy node that points to head
+// declare a variable currentValue with initial value of null
+// declare a variable prev with initial value of dummy
+// traverse the list until head.next is null
+// check if the value of head is the same as currentValue
+// if it is prev points to the next value and start again
+// if it's not currentValue gets the value of head
+// check if the next value is the same as currentValue
+// if it is prev next points to next next
+// and start again
+// if it is not
+// prev gets head and head gets next
+// and keep going
