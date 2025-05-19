480eb197a34223fdea9b73c68392fff5129b5417
Kasper Bak <dan.echevarri@gmail.com>
2025-05-19 11:18:25 +0200

    Add suboptimal solution

--- Javascript/543-diameter-of-binray-tree.js
+++ Javascript/543-diameter-of-binray-tree.js
@@ -14,31 +14,39 @@
 // longestPath gets the max longestLeft and longestRight
 // return longest
 
-// const maxDepth = (root) => {
-//   if (!root) return 0;
-//   if (!root.left && !root.right) return 1;
-//   const maxLeft = root.left ? 1 + maxDepth(root.left) : 0;
-//   const maxRight = root.right ? 1 + maxDepth(root.right) : 0;
-//   return Math.max(maxLeft, maxRight);
-// };
+const maxDepth = (root) => {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  const maxLeft = root.left ? 1 + maxDepth(root.left) : 0;
+  const maxRight = root.right ? 1 + maxDepth(root.right) : 0;
+  return Math.max(maxLeft, maxRight);
+};
 
-// const getDiam = (root) => {
-//   if (!root) return 0;
-//   const longestLeft = root.left ? maxDepth(root.left) : 0;
-//   const longestRight = root.right ? maxDepth(root.right) : 0;
-//   return longestLeft + longestRight;
-// };
+const getDiam = (root) => {
+  if (!root) return 0;
+  const longestLeft = root.left ? maxDepth(root.left) : 0;
+  const longestRight = root.right ? maxDepth(root.right) : 0;
+  return longestLeft + longestRight;
+};
 
-// var diameterOfBinaryTree = function (root) {
-//   if (!root) return 0;
-//   let maxDiam = 0;
-//   const getMaxDiam = (root) => {
-//     const maxLeft = getDiam(root.left);
-//     const maxRight = getDiam(root.right);
-//     const current = getDiam(root);
-//     maxDiam = Math.max(maxLeft, maxRight, current);
-//   };
-//   const maxDiamLeft = root.left ? getMaxDiam(root.left) : 0;
-//   const maxDiamRight = root.right ? getMaxDiam(root.right) : 0;
+var diameterOfBinaryTree = function (root) {
+  if (!root) return 0;
+  let maxDiam = 0;
+  const getMaxDiam = (root) => {
+    const maxLeft = getDiam(root.left);
+    const maxRight = getDiam(root.right);
+    const current = getDiam(root);
+    maxDiam = Math.max(maxLeft, maxRight, current, maxDiam);
+  };
+  if (root.left) getMaxDiam(root.left);
+  if (root.right) getMaxDiam(root.right);
+  getMaxDiam(root);
+  return maxDiam;
+};
 
-// };
+// Algo diameter
+// get the maxDiameter from the left child
+//
+// get the maxDiameter from the right chid
+// get the diameter of the current node
+// return the biggest of the 3 results
