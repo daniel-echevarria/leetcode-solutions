8cc5f87ce4a3d66f90f3284de08f089a5d67cd0a
Kasper Bak <dan.echevarri@gmail.com>
2025-05-26 16:17:09 +0200

    Add solution

--- Javascript/110-balance-binary-tree.js
+++ Javascript/110-balance-binary-tree.js
@@ -1,1 +1,17 @@
-var isBalanced = function (root) {};
+var isBalanced = function (root) {
+  let isBalanced = true;
+  const DFS = (root) => {
+    if (!root) return 0;
+    const left = 1 + DFS(root.left);
+    const right = 1 + DFS(root.right);
+    if (Math.abs(left - right) > 1) isBalanced = false;
+    const maxHeight = Math.max(left, right);
+    return maxHeight;
+  };
+  DFS(root);
+  return isBalanced;
+};
+
+// Algo
+// Traverse the tree DFS checking the height of the left and right children
+// If at any stage the absolute difference is bigger than 1 return false
