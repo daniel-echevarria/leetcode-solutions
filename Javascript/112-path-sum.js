87ae21f15a415cde75e2adbf450951e8f8464079
Kasper Bak <dan.echevarri@gmail.com>
2025-05-27 14:20:06 +0200

    Add optimal solution

--- Javascript/112-path-sum.js
+++ Javascript/112-path-sum.js
@@ -1,19 +1,28 @@
-var hasPathSum = function (root, targetSum) {
-  let hasTargetSum = false;
-  const DFS = (root, sum = 0) => {
-    if (hasTargetSum) return;
-    if (!root) return;
-    sum = sum + root.val;
-    if (!root.left && !root.right && sum === targetSum) {
-      hasTargetSum = true;
-      return;
-    }
+// var hasPathSum = function (root, targetSum) {
+//   let hasTargetSum = false;
+//   const DFS = (root, sum = 0) => {
+//     if (hasTargetSum) return;
+//     if (!root) return;
+//     sum = sum + root.val;
+//     if (!root.left && !root.right && sum === targetSum) {
+//       hasTargetSum = true;
+//       return;
+//     }
 
-    DFS(root.left, sum);
-    DFS(root.right, sum);
-  };
-  DFS(root);
-  return hasTargetSum;
+//     DFS(root.left, sum);
+//     DFS(root.right, sum);
+//   };
+//   DFS(root);
+//   return hasTargetSum;
+// };
+
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+  targetSum = targetSum - root.val;
+  if (!root.left && !root.right && targetSum == 0) return true;
+  const left = hasPathSum(root.left, targetSum);
+  const right = hasPathSum(root.right, targetSum);
+  return left || right;
 };
 
 // Algo
