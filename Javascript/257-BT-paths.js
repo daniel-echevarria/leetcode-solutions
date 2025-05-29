b9eb4c6fb268c0cea341cdfa4ed3a2d64ff8ad88
Kasper Bak <dan.echevarri@gmail.com>
2025-05-29 15:55:36 +0200

    Add optimal solution

--- Javascript/257-BT-paths.js
+++ Javascript/257-BT-paths.js
@@ -2,14 +2,12 @@ var binaryTreePaths = function (root) {
   const paths = [];
   const DFS = (root, path = "") => {
     if (!root) return;
-    path += root.val;
     if (!root.left && !root.right) {
-      paths.push(path);
+      paths.push(path + root.val);
       return;
     }
-    path += "->";
-    DFS(root.left, path);
-    DFS(root.right, path);
+    DFS(root.left, path + root.val + "->");
+    DFS(root.right, path + root.val + "->");
   };
   DFS(root);
   return paths;
