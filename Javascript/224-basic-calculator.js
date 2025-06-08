afabab12eb34145c0ab96d69f6f1522d6161aadd
Kasper Bak <dan.echevarri@gmail.com>
2025-06-08 12:23:12 +0200

    Add 129 and 224

---
+++ Javascript/224-basic-calculator.js
@@ -0,0 +1,5 @@
+var calculate = function (s) {};
+
+// First clean the string (remove spaces)
+// then split the string into characters
+// then iterate through the array keeping a stack and a result so far
