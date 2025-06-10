69e33eba4cd03695f59dcd26008d0f7a428df120
Kasper Bak <dan.echevarri@gmail.com>
2025-06-10 19:34:55 +0200

    Add suboptimal solution

--- Javascript/114-flatten.js
+++ Javascript/114-flatten.js
@@ -2,18 +2,30 @@ var flatten = function (root) {
   const preOrder = [];
   const DFS = (node) => {
     if (!node) return;
-    preOrder.push(node.val);
+    preOrder.push(node);
     DFS(node.left);
     DFS(node.right);
   };
   DFS(root);
-  let dummy = new TreeNode(0);
-  let currentNode = new TreeNode(preOrder.shift());
-  dummy.right = currentNode;
-  while (preOrder.length) {
-    currentNode.right = new TreeNode(preOrder.shift());
-    currentNode = currentNode.right;
+  for (let i = 0; i < preOrder.length; i++) {
+    const currentNode = preOrder[i];
+    currentNode.left = null;
+    currentNode.right = i < preOrder.length - 1 ? preOrder[i + 1] : null;
   }
-  root = dummy.right;
   return root;
 };
+
+// const flatten = (root) => {
+//   const DFS = (node) => {
+//     if (!node) return;
+//     if (node.left.left === null && node.left.rightl)
+//   };
+// };
+
+// If the left node is a leaf
+// store the right node in a tempo
+// right gets left
+// left gets null
+// right right gets tempo
+// otherwise call flatten on the left
+// call flatten on the right
