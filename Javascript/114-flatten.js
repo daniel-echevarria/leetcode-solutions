2dd6a63604fb0ddf64764f183f1d8e07a56509f5
Kasper Bak <dan.echevarri@gmail.com>
2025-06-10 15:37:24 +0200

    First attempt

--- Javascript/114-flatten.js
+++ Javascript/114-flatten.js
@@ -1,1 +1,19 @@
-var flatten = function (root) {};
+var flatten = function (root) {
+  const preOrder = [];
+  const DFS = (node) => {
+    if (!node) return;
+    preOrder.push(node.val);
+    DFS(node.left);
+    DFS(node.right);
+  };
+  DFS(root);
+  let dummy = new TreeNode(0);
+  let currentNode = new TreeNode(preOrder.shift());
+  dummy.right = currentNode;
+  while (preOrder.length) {
+    currentNode.right = new TreeNode(preOrder.shift());
+    currentNode = currentNode.right;
+  }
+  root = dummy.right;
+  return root;
+};
