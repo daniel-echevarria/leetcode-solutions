a59dfe2754c0a9531597a8822b66c028a6bb6a53
Kasper Bak <dan.echevarri@gmail.com>
2025-06-14 11:26:45 +0200

    Add solution

--- Javascript/129-sum-root-to-leaf.js
+++ Javascript/129-sum-root-to-leaf.js
@@ -1,1 +1,14 @@
-var sumNumbers = function (root) {};
+var sumNumbers = function (root) {
+  let total = 0;
+  const DFS = (node, string = "") => {
+    if (!node) return;
+    if (!node.left && !node.right) {
+      total += +(string + node.val);
+      return;
+    }
+    DFS(node.left, string + node.val);
+    DFS(node.right, string + node.val);
+  };
+  DFS(root);
+  return total;
+};
