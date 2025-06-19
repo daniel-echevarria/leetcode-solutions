50ca08f21f07ffde2b12926475f2fc709bc94812
Kasper Bak <dan.echevarri@gmail.com>
2025-06-19 18:49:35 +0200

    Add problem 230 with solution

--- Javascript/230-kth-smallest-element.js
+++ Javascript/230-kth-smallest-element.js
@@ -1,1 +1,18 @@
-var kthSmallest = function (root, k) {};
+var kthSmallest = function (root, k) {
+  let count = 0;
+  let solution = null;
+  const DFS = (node) => {
+    if (solution !== null) return;
+    if (!node) return;
+
+    DFS(node.left);
+    count += 1;
+    if (count === k) {
+      solution = node.val;
+      return;
+    }
+    DFS(node.right);
+  };
+  DFS(root);
+  return solution;
+};
