ce176e6ec0f138181ff6e7a2c7f0ed2d3cfa0579
Kasper Bak <dan.echevarri@gmail.com>
2025-06-27 13:36:07 +0200

    Get closer to the solution

--- Javascript/76-min-window-sub.js
+++ Javascript/76-min-window-sub.js
@@ -1,54 +1,39 @@
 var minWindow = function (s, t) {
-  const m = s.length;
-  const n = t.length;
-  let left = 0;
-  let right = n;
-  let minWindow = s;
-  let encounteredAt = new Map();
-  while (s.substring(left).length > n) {
-    let allThere = false;
-    const window = s.substring(left, right);
-    for (let i = 0; i < n; i++) {
-      const index = window.indexOf(encounteredAt.get(char) || 0);
-      if (index === -1) {
-        left++;
-        encounteredAt = new Map();
-        break;
-      }
-      encounteredAt.set(char, i + 1);
-      if
+  const n = s.length;
+  const m = t.length;
+  if (n < m) return "";
+  if (s == t) return t;
+  let l = 0;
+  let r = 0 + m;
+  let minWindowSize = s.length;
+  let minWindow = "";
+  let myMap = new Map();
+  for (let i = 0; i < n; i++)
+    myMap.get(s[i]) ? myMap.get(s[i]).push(i) : myMap.set(s[i], [i]);
+  const sArray = s.split("");
+  while (l < n) {
+    const window = sArray.slice(l, r);
+    let allInclusive = true;
+    for (let char of t) {
+      console.log(char);
+      const indexes = myMap.get(char);
+      console.log(indexes);
+      const possibles = indexes.filter((idx) => idx >= l && idx <= r);
+      console.log(possibles);
+      if (indexes.length == 0) allInclusive = false;
+      // if (window.includes(char)) {
+      //   const idx = window.indexOf(char);
+      //   window[idx] = "#";
+      //   continue;
+      // }
+      // allInclusive = false;
     }
-    console.log(allThere);
-    if (allThere) {
-      minWindow = window.length < minWindow.length ? window : minWindow;
-      left++;
-      continue;
+    if (allInclusive && window.length <= minWindowSize) {
+      minWindow = s.slice(l, r);
+      minWindowSize = window.length;
     }
-    right++;
+    allInclusive ? l++ : r++;
+    if (r == n + 1 && !allInclusive) break;
   }
   return minWindow;
 };
-
-console.log(minWindow("ADOBECODEBANC", "ABC"));
-
-// Algo
-// m = s.length
-// n = t.length
-// declare a variable left with initial value of 0
-// declare a variable right with initial value of n
-// declare a variable minWindow with initial value of m
-// declare a map encounteredAt
-// declare a variable allThere with initial value of false
-// split t into an array of characters
-// launch a loop
-// given a window which is a substring of s from left to right
-// iterate through the characters
-// find the index of the character in the window starting at the value from encounteredAt or 0
-// if index is -1 stop iteration
-// otherwise add that character as a key to encountered with a value of the index + 1
-// after iteration allThere gets true
-// if allThere is true minWindow gets the smallest of the two values minWindow or currentWindowLength
-// and left gets ++
-// otherwise
-// right get ++
-// stop the loop when the substring left to end is smaller than n
