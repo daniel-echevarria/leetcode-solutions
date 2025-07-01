c417dbc4f0ab08dbf5a2046ba0a77958ffee9a12
Kasper Bak <dan.echevarri@gmail.com>
2025-07-01 12:55:53 +0200

    Add different solution

--- Javascript/997-find-judge.js
+++ Javascript/997-find-judge.js
@@ -1,22 +1,22 @@
-var findJudge = function (n, trust) {
-  const labels = Array.from({ length: n }, (_, i) => i + 1);
-  const trustGraph = {};
-  for (const pair of trust) {
-    const a = pair[0];
-    const b = pair[1];
-    trustGraph[a] ? trustGraph[a].push(b) : (trustGraph[a] = [b]);
-  }
-  console.log(trustGraph);
-  const trusting = Object.keys(trustGraph).map((key) => +key);
-  const judges = labels.filter((label) => !trusting.includes(label));
-  if (judges.length != 1) return -1;
-  const judge = judges[0];
-  for (people in trustGraph) {
-    if (trustGraph[people].includes(judge)) continue;
-    return -1;
-  }
-  return judge;
-};
+// var findJudge = function (n, trust) {
+//   const labels = Array.from({ length: n }, (_, i) => i + 1);
+//   const trustGraph = {};
+//   for (const pair of trust) {
+//     const a = pair[0];
+//     const b = pair[1];
+//     trustGraph[a] ? trustGraph[a].push(b) : (trustGraph[a] = [b]);
+//   }
+//   console.log(trustGraph);
+//   const trusting = Object.keys(trustGraph).map((key) => +key);
+//   const judges = labels.filter((label) => !trusting.includes(label));
+//   if (judges.length != 1) return -1;
+//   const judge = judges[0];
+//   for (people in trustGraph) {
+//     if (trustGraph[people].includes(judge)) continue;
+//     return -1;
+//   }
+//   return judge;
+// };
 
 // Make an array of labels from 1 to n
 // Iterate through trust and make a graph of the trust
@@ -49,14 +49,6 @@ var findJudge = function (n, trust) {
 //   return judge;
 // };
 
-console.log(
-  findJudge(3, [
-    [1, 3],
-    [2, 3],
-    [3, 1],
-  ])
-);
-
 // New Algo
 // Declare a variable judge with initial value of -1
 // Iterate from 1 to n
@@ -65,3 +57,22 @@ console.log(
 // If one label has no pair, judge gets i or return -1 if judge was already selected
 // After iteration check if all the people in the graph trust the judge
 // if yest return the judge otherwise return -1
+
+var findJudge = function (n, trust) {
+  if (n == 1) return 1;
+  const howManyTrusts = {};
+  const trusting = new Set();
+  let judge = null;
+  let maxTrust = 0;
+  for (const pair of trust) {
+    a = pair[0];
+    b = pair[1];
+    trusting.add(a);
+    const totalTrusts = (howManyTrusts[b] || 0) + 1;
+    howManyTrusts[b] = totalTrusts;
+    if (totalTrusts > maxTrust) judge = b;
+    maxTrust = Math.max(maxTrust, totalTrusts);
+  }
+  if (howManyTrusts[judge] == n - 1 && !trusting.has(judge)) return judge;
+  return -1;
+};
