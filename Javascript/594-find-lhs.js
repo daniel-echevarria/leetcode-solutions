70029351107eb68d484a05ac539940839fe1253b
Kasper Bak <dan.echevarri@gmail.com>
2025-07-01 12:56:31 +0200

    Add problem 594

---
+++ Javascript/594-find-lhs.js
@@ -0,0 +1,8 @@
+var findLHS = function (nums) {};
+
+// Order the array
+//
+// Declare variables l, r with initial values of 0 and 1
+// If the difference between nums[l] dn nums[r] is bigger than 1 return 0
+// Loop until l equals nums length - 1
+// Keep updating
