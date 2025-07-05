602fdc1876698c84ad85635faa01a64d0d47fd37
Kasper Bak <dan.echevarri@gmail.com>
2025-07-05 14:19:29 +0200

    Fix code but still too slow

--- Javascript/207-course-schedule.js
+++ Javascript/207-course-schedule.js
@@ -10,25 +10,28 @@ var canFinish = function (numCourses, prerequisites) {
     needsGraph[can] = [must];
   }
   let loopExists = false;
-  const visited = new Set();
+  let visited = new Set();
   const BFS = (current, originalCourse) => {
-    if (visited.has(current) || !needsGraph[current]) return;
+    if (visited.has(current) || !needsGraph[current] || loopExists) return;
     visited.add(current);
     const needs = needsGraph[current];
     if (needs.includes(Number(originalCourse))) loopExists = true;
     for (n of needs) {
       BFS(n, originalCourse);
+      visited = new Set();
     }
   };
-  for (const key in Object.keys(needsGraph)) {
+  for (const key of Object.keys(needsGraph)) {
     BFS(key, key);
   }
   return !loopExists;
 };
 
-// Iterate through prerequisites and make a map of the needsGraph
+// through prerequisites and make a map of the needsGraph
 // Then iterate through the keys of the map and operate a Breadth first search
 // that looks for the same value than the key
 // Declare a set visited to keep track and avoid infinite loops
 // if the bfs returns false return false
 // after iteration return true
+
+//  New Approach
