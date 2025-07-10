a376114038697ad92c16eae4d5bf8cdf95c9d0e4
Kasper Bak <dan.echevarri@gmail.com>
2025-07-10 18:31:07 +0200

    Optimize algo

--- Javascript/797-all-paths-source-target.js
+++ Javascript/797-all-paths-source-target.js
@@ -1,22 +1,17 @@
 var allPathsSourceTarget = function (graph) {
-  const n = graph.length;
   const allPaths = [];
-  const DFS = (node, path = []) => {
-    path.push(node);
-    if (path.includes(n - 1)) {
-      allPaths.push(path);
+  const path = [0];
+  const DFS = (node) => {
+    if (node == graph.length - 1) {
+      allPaths.push([...path]);
       return;
     }
-    if (graph[node].length < 1) return;
     for (const link of graph[node]) {
-      DFS(link, [...path]);
+      path.push(link);
+      DFS(link, path);
+      path.pop();
     }
   };
   DFS(0);
   return allPaths;
 };
-
-// Declare an empty array allPaths
-// Traverse the graph BFS style while keeping track of the visited nodes
-// If there is no node you can visit from the current node, push the path to the allPaths
-// After traversal return allPaths
