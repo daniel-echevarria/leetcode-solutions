bea00e14201229e2814fee339dd7153c9ed4cbf5
Kasper Bak <dan.echevarri@gmail.com>
2025-07-12 16:15:24 +0200

    New attempt

--- Javascript/200-number-of-islands.js
+++ Javascript/200-number-of-islands.js
@@ -1,4 +1,27 @@
-var numIslands = function (grid) {};
+var numIslands = function (grid) {
+  const graph = generateAgencyList(grid);
+  const visited = new Set();
+  const queue = [graph];
+};
 
-// Iterate through the grid
-// for each row
+// Create a graph of agency with x,y coordinates
+// where each coordinate gets a value and neighbors
+const generateAgencyList = (grid) => {
+  const height = grid.length;
+  const width = grid[0].length;
+  const adjacencyList = {};
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      adjacencyList[`${i},${j}`] = {
+        val: grid[i][j],
+        neighbors: [
+          [i - 1, j],
+          [i + 1, j],
+          [i, j - 1],
+          [i, j + 1],
+        ],
+      };
+    }
+  }
+  return adjacencyList;
+};
