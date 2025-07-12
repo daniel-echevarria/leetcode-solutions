c2b5410d7b37c99f306d250ede943d5c8842558b
Kasper Bak <dan.echevarri@gmail.com>
2025-07-12 16:15:38 +0200

    New attempt

--- Javascript/207-course-schedule.js
+++ Javascript/207-course-schedule.js
@@ -1,70 +1,36 @@
-// var canFinish = function (numCourses, prerequisites) {
-//   const needsGraph = {};
-//   for (const pre of prerequisites) {
-//     const can = pre[0];
-//     const must = pre[1];
-//     if (needsGraph[can]) {
-//       needsGraph[can].push(must);
-//       continue;
-//     }
-//     needsGraph[can] = [must];
-//   }
-//   let loopExists = false;
-//   let visited = new Set();
-//   const BFS = (current, originalCourse) => {
-//     if (visited.has(current) || !needsGraph[current] || loopExists) return;
-//     visited.add(current);
-//     const needs = needsGraph[current];
-//     if (needs.includes(Number(originalCourse))) loopExists = true;
-//     for (n of needs) {
-//       BFS(n, originalCourse);
-//       visited = new Set();
-//     }
-//   };
-//   for (const key of Object.keys(needsGraph)) {
-//     BFS(key, key);
-//   }
-//   return !loopExists;
-// };
-
-const canFinish = (numCourses, prerequisites) => {
-  const mustTake = {};
-  const mustBeTakenBy = {};
-  for (const pre of prerequisites) {
-    const a = pre[0];
-    const b = pre[1];
-    mustTake[a] ? mustTake[a].push(b) : (mustTake[a] = [b]);
-    mustBeTakenBy[b] ? mustBeTakenBy[b].push(a) : (mustBeTakenBy[b] = [a]);
+var canFinish = function (numCourses, prerequisites) {
+  const graph = {};
+  for (const pair of prerequisites) {
+    const [a, b] = pair;
+    graph[a] ? graph[a].push(b) : (graph[a] = [b]);
   }
-  for (const [course, prerequisites] of Object.entries(mustBeTakenBy)) {
-    for (const pre of prerequisites) {
-      if (mustBeTakenBy[pre]) {
-        for (const must of mustBeTakenBy[pre]) {
-          mustTake[must].push(Number(course));
-        }
-      }
+  let visited = new Set();
+  const dfs = (node) => {
+    if (!graph[node]) return true;
+    if (visited.has(node)) return false;
+    visited.add(node);
+    for (const pre of graph[node]) {
+      if (dfs(pre)) continue;
+      return false;
     }
+  };
+  let noLoop = true;
+  for (let i = 0; i < numCourses; i++) {
+    visited = new Set();
+    if (dfs(i)) continue;
+    noLoop = false;
   }
-  console.log(mustTake, mustBeTakenBy);
-  for (const course in mustTake) {
-    if (mustTake[course].includes(Number(course))) return false;
-  }
-  return true;
+  return noLoop;
 };
 
-const resultMustTake = {
-  1: [0, 3, 0],
-  2: [0],
-  3: [1, 2],
-};
-
-// through prerequisites and make a map of the needsGraph
-// Then iterate through the keys of the map and operate a Breadth first search
-// that looks for the same value than the key
-// Declare a set visited to keep track and avoid infinite loops
-// if the bfs returns false return false
-// after iteration return true
-
-//  New Approach
-//  Create a both a map of mustTake and mustBeTakenBy
-//  When encountering  a new mustTake
+// Iterate through the courses in the graph and traverse them DFS
+// if the course as no requisites return true
+// if the course as a requisites that was already visited return false
+// recursively call dfs on all the requisites
+canFinish(2, [
+  [2, 0],
+  [1, 0],
+  [3, 1],
+  [3, 2],
+  [1, 3],
+]);
