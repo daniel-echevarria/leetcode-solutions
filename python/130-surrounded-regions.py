ef93014b6785720627378bfce10968851ea03131
Kasper Bak <dan.echevarri@gmail.com>
2025-07-15 15:50:05 +0200

    Add problem 130 with solution

---
+++ python/130-surrounded-regions.py
@@ -0,0 +1,49 @@
+class Solution:
+    def solve(self, board: list[list[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+        visited = set()
+
+        def isOutOfBounds(i, j):
+            if i < 0 or j < 0 or i > m - 1 or j > n - 1:
+                return True
+            return False
+
+        def isRegion(i, j):
+            coordinate_string = f"${i}${j}"
+            if isOutOfBounds(i, j):
+                return False
+            if board[i][j] == 'X' or coordinate_string in visited:
+                return True
+            visited.add(coordinate_string)
+            return isRegion(i + 1, j) and isRegion(i - 1, j) and isRegion(i, j + 1) and isRegion(i, j - 1)
+
+        def capture(i, j):
+            if isOutOfBounds(i, j) or board[i][j] == 'X':
+                return
+            board[i][j] = 'X'
+            capture(i + 1, j)
+            capture(i - 1, j)
+            capture(i, j + 1)
+            capture(i, j - 1)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'X':
+                    continue
+                if isRegion(i, j):
+                    capture(i, j)
+                else:
+
+                    visited = set()
+
+s = Solution()
+s.solve([["O","O"],["O","O"]])
+
+# Iterate through the matrix
+# if the value is X, continue
+# if the value is O, look if the region is surrounded (no neighbor is out of bounds)
+# if it is call dfs again to capture the region
