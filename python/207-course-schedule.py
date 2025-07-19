b12b4f56ed97e457bf0f92fb8eb68914a44de1fa
Kasper Bak <dan.echevarri@gmail.com>
2025-07-19 12:09:12 +0200

    Add solution

--- python/207-course-schedule.py
+++ python/207-course-schedule.py
@@ -1,27 +1,49 @@
+from collections import defaultdict
+
+
+def create_graph(prerequisites):
+    graph = defaultdict(list)
+    for pre in prerequisites:
+        a, b = pre
+        graph[a].append(b)
+    return graph
+
+
+def dfs(course, visited, graph, fine):
+    if len(graph[course]) == 0 or course in fine:
+        return False
+    if course in visited:
+        return True
+    visited.add(course)
+    for neighbor in graph[course]:
+        loop_exits = dfs(neighbor, visited, graph, fine)
+        if loop_exits:
+            return True
+        fine.add(neighbor)
+    return False
+
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
-        graph = {}
-        for a, b in prerequisites:
-            if a in graph:
-                graph[a].append(b)
-            else:
-                graph[a] = [b]
-        visited = set
-
-    def dfs(node, graph, visited):
-        if node not in graph or len(graph[node]) == 0:
-            return True
-        if node in visited:
-            return False
-        visited.add(node)
-        for musts in graph[node]:
-            dfs(node, graph, visited)
+        graph = create_graph(prerequisites)
+        fine = set()
+        for course in range(numCourses):
+            visited = set()
+            loop_exits = dfs(course, visited, graph, fine)
+            if loop_exits:
+                return False
+            fine.add(course)
+        return True
 
 
+s = Solution()
+num = 8
+pre = [[1, 0], [0, 3], [0, 2], [3, 2], [2, 5], [4, 5], [5, 6], [2, 4]]
 
+s.canFinish(num, pre)
 
-# Iterates through requisites and make a graph of each prerequisites for each course
-# then DFS each course looking for loops
-# if a loop is found return false
-
-# if no loop is found return true
+# Algo
+# First build a graph of the mustTake for each course
+# Then iterate from 0 and see if there is any loop
+# If there is a loop return false
+# Otherwise return true
