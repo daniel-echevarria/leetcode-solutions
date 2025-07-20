c4dbd6e2c48585522739bdaef88291ba3b349ba2
Kasper Bak <dan.echevarri@gmail.com>
2025-07-20 18:02:24 +0200

    Add problem 210

--- python/210-course-schedule-2.py
+++ python/210-course-schedule-2.py
@@ -17,24 +17,50 @@ def createMustBeTakenGraph(prerequisites):
     return graph
 
 
+def dfs2(node, must_take, order, visited=set()):
+    if node in visited:
+        return
+    visited.add(node)
+    neighbors = must_take[node]
+    if all(neighbor in order for neighbor in neighbors) or len(must_take[node]) == 0:
+        order.append(node)
+        return
+    for neighbor in must_take[node]:
+        dfs2(neighbor, must_take, order)
+
+
+def dfs(node, must_take, must_be_taken, order):
+    if not node in order:
+        order.append(node)
+    dependents = must_be_taken[node]
+    for course in dependents:
+        dfs2(course, must_take, order)
+
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
         must_take = createMustTakeGraph(prerequisites)
         must_be_taken = createMustBeTakenGraph(prerequisites)
         starts = [course for course in range(numCourses) if len(must_take[course]) == 0]
-        print(starts)
+        order = []
+        for start in starts:
+            dfs(start, must_take, must_be_taken, order)
+        return order
 
 
 s = Solution()
-numCourses = 2
-pres = [[1, 0]]
+numCourses = 4
+pres = [[1, 0], [2, 0], [3, 1], [3, 2]]
+
 s.findOrder(numCourses, pres)
 
 
 # Create a graph of the mustTake and mustBeTakenBy
-# Find one course that mustTake nothing
-# Launch a dfs on that course for the mustBeTakenBy while pushing into order
-# delete that starting point from the graph
-# do the same for the next mustTake nothing
+# make an array of all the starting points (their must_take is empty)
+# Launch a BFS on each of the starting points as such:
+# push the current node the the order list unless it's already there
+# if there is no neighbors return
+# otherwise for each neighbor, check what courses they must take,
+# If these courses are already in order, push the neighbor to order and continue
+# otherwise call dfs on each neighbor, if there is a loop exit and return an empty array
 # if order is as long as numCourses return order
-# if there is no more mustTake nothing return empty array
