3982beb5010cc8fb3b7f6c91550e867e1a969742
Kasper Bak <dan.echevarri@gmail.com>
2025-08-05 20:58:58 +0200

    Bang my head against the walls

--- python/224-basic-calculator.py
+++ python/224-basic-calculator.py
@@ -13,36 +13,72 @@ class Solution:
         def add_or_subtract(operator, a, b):
             if operator == "+":
                 return f"{int(a) + int(b)}"
-
             if operator == "-":
                 return f"{int(a) - int(b)}"
 
+        for i in range(1, len(array)):
+            if array[i].isdigit() and array[i - 1].isdigit():
+                array[i - 1] += array[i]
+                array[i] = "#"
+
         for el in array:
-            if el.isdigit() and stack and stack[-1] in operators:
-                if stack[-2].isdigit():
+            if el == "#":
+                continue
+            if el.isdigit() and stack and stack[-1].lstrip("-").isdigit():
+                first_digit = stack.pop()
+                stack.append(first_digit + el)
+            elif el.isdigit() and stack and stack[-1] in operators:
+                if len(stack) == 1:
+                    _ = stack.pop()
+                    stack.append(f"{int(el) * -1}")
+                elif len(stack) > 1 and not stack[-2].lstrip("-").isdigit():
+                    _ = stack.pop()
+                    stack.append(f"{int(el) * -1}")
+                else:
                     operator = stack.pop()
                     left = stack.pop()
                     stack.append(add_or_subtract(operator, left, el))
+
             elif el == ")":
+                print(stack)
                 right = stack.pop()
-                _ = stack.pop()
+                stack.pop()
                 if not stack:
                     stack.append(right)
                 elif len(stack) == 1:
-                    stack.pop()
-                    stack.append(add_or_subtract(right * -1))
+                    stack.append(f"{int(right) * -1}")
+                elif len(stack) == 2:
+                elif stack[-1] == "(":
+
+                    continue
                 else:
                     operator = stack.pop()
                     left = stack.pop()
                     stack.append(add_or_subtract(operator, left, right))
+
             else:
                 stack.append(el)
-        return int(stack[0])
+        res = int(stack[0])
+        print(res)
+        return res
 
 
 my_string = "(1+(4+5+2)-3)+(6+8)"
+my_string_2 = "  30"
+my_string_3 = "1-(     -2)"
+four = "-2+ 1"
+fuck = "- (3 + (4 + 5))"
+peak = "1-11"
+damn = "1 + ((1 + 1) + 1)"
+
 s = Solution()
 s.calculate(my_string)
+s.calculate(my_string_2)
+s.calculate(my_string_3)
+s.calculate(four)
+s.calculate(fuck)
+s.calculate(peak)
+s.calculate(damn)
 
 
 # Remove the white space from the string
