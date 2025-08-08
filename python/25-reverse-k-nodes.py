8c4dd63fa7b6448233cac4a80fb00ac9ab6fc306
Kasper Bak <dan.echevarri@gmail.com>
2025-08-08 11:30:01 +0200

    Fix file name

--- python/25-reverse-k-nodes.py
+++ python/25-reverse-k-nodes.py
@@ -5,3 +5,20 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+# Algo
+# Keep track of the pre_reversing node and the post_reversing node
+# Make a dummy head to be able to return the modified list in the end
+# Declare a current var with initial value of head
+# prev gets dummy
+# launch a loop until current next is null
+# inside the loop reverse the nodes as such:
+# declare a list of reversing
+# launch a for loop pushing the nodes to reversing and updating current
+# after the loop post gets current next
+# point prev next to the reversing last
+# pop them one by one pointing to reversing last
+# prev gets current
+# current next gets post
+# keep going
+
