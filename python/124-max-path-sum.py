3f7146665af22f69be9cc62823385148cd2fecc3
Kasper Bak <dan.echevarri@gmail.com>
2025-08-10 11:19:45 +0200

    Solve problem 124

--- python/124-max-path-sum.py
+++ python/124-max-path-sum.py
@@ -6,3 +6,23 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.max_sum = float("-inf")
+
+        def dfs(node):
+            if not node:
+                return float("-inf")
+            if not node.left and not node.right:
+                return node.val
+            max_left = dfs(node.left)
+            max_right = dfs(node.right)
+            self.max_sum = max(
+                node.val,
+                max_left,
+                max_right,
+                (max_left + max_right + node.val),
+                self.max_sum,
+            )
+            return max(max(max_left, max_right) + node.val, node.val)
+
+        root_max = dfs(root)
+        return max(self.max_sum, root_max)
