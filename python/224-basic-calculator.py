1495ff56d4b043f368ef9ea3c66260c40d38120c
Kasper Bak <dan.echevarri@gmail.com>
2025-09-10 10:16:49 +0200

    Add solution

--- python/224-basic-calculator.py
+++ python/224-basic-calculator.py
@@ -1,64 +1,23 @@
-from collections import deque
-import re
-
-
 class Solution:
     def calculate(self, s: str) -> int:
-        # clean = list("".join(s.split("+")))
-        # print(clean)
-        clean = list(s)
-        print(clean)
-        de = deque([])
-        for char in clean:
-            if char == "(" or char == "-" or char == "+":
-                de.append(char)
-            elif char.isnumeric():
-                if len(de) == 0 or de[-1] == "(":
-                    de.append(int(char))
-                elif de[-1] == "-":
-                    de.pop()
-                    de.append(int(char) * -1)
-                elif de[-1] == "+":
-                    de.pop()
-                    prev = de.pop()
-                    de.append(int(char) + prev)
-                else:
-                    prev = de.pop()
-                    de.append(f"{prev}{char}")
-            elif char == ")":
-                c = char
-                res = 0
-                while True:
-                    c = de.pop()
-                    if c == "(":
-                        if len(de) == 0 or not de[-1] == "-":
-                            de.append(res)
-                            break
-                        de.pop()
-                        de.append(res * -1)
-                        break
-                    res += int(c)
-        print(de)
-        without_plus = filter(lambda x: x != "+", de)
-        final = map(lambda x: int(x), without_plus)
-        result = sum(final)
-        print(result)
+        def helper(it):
+            total, sign, num = 0, 1, 0
+            for char in it:
+                if char == " ":
+                    continue
+                elif char.isdigit():
+                    num = num * 10 + int(char)
+                elif char in "-+":
+                    total += num * sign
+                    num = 0
+                    sign = 1 if char == "+" else -1
+                elif char == "(":
+                    total += helper(it) * sign
+                    num = 0
+                elif char == ")":
+                    total += num * sign
+                    return total
+            return total + num * sign
+
+        result = helper(iter(s))
         return result
-
-
-ex1 = "(1-(4+5+2)-3)+(6+8)"
-ex2 = "1 + 1"
-ex3 = " 2-1 + 2 "
-ex4 = "2147483647"
-
-s = Solution()
-s.calculate(ex1)
-s.calculate(ex2)
-s.calculate(ex3)
-s.calculate(ex4)
-# algo
-# split the string
-# declare a stack
-# iterate through the splitted string and push the elements following this logic:
-# if it's a white space, continue (ignore)
-#
