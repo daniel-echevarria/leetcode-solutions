3378623b176d1e1ed95d80abe898aaf0baedcc80
Kasper Bak <dan.echevarri@gmail.com>
2025-09-17 12:42:03 +0200

    Add coordinates translation

--- python/909-snake-and-ladders.py
+++ python/909-snake-and-ladders.py
@@ -1,18 +1,19 @@
 class Solution:
+    def numToCoordinates(self, num: int, board_size: int):
+        row_from_bottom = (num - 1) // board_size
+        row = board_size - 1 - row_from_bottom
+        col = (num - 1) % board_size
+        if row_from_bottom % 2 != 0:
+            col = board_size - 1 - col
+        print(row, col)
+        return (row, col)
+
     def snakesAndLadders(self, board: list[list[int]]) -> int:
-        n = len(board)
-        gameBoard = [list(range(n)) for _ in range(n)]
-        square_num = 1
-        for row in range(n):
-            for col in range(n):
-                gameBoard[row][col] = square_num
-                square_num += 1
-        for row in range(n):
-            if row % 2 != 0:
-                gameBoard[row].reverse()
+        print("yo")
 
 
 s = Solution()
+s.numToCoordinates(13, 4)
 s.snakesAndLadders(
     [
         [-1, -1, -1, -1, -1, -1],
