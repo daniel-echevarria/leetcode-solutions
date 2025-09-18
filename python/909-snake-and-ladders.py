0aff3dd5093a8c78216bd0214144696725410f16
Kasper Bak <dan.echevarri@gmail.com>
2025-09-18 10:55:15 +0200

    Add valid solution

--- python/909-snake-and-ladders.py
+++ python/909-snake-and-ladders.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Solution:
     def numToCoordinates(self, num: int, board_size: int):
         row_from_bottom = (num - 1) // board_size
@@ -5,24 +8,46 @@ class Solution:
         col = (num - 1) % board_size
         if row_from_bottom % 2 != 0:
             col = board_size - 1 - col
-        print(row, col)
         return (row, col)
 
     def snakesAndLadders(self, board: list[list[int]]) -> int:
-        print("yo")
+        n = len(board)
+        goal = n * n
+        visited = [False] * goal
+        queue = deque([1])
+        rolls = 0
+        while queue:
+            rolls += 1
+            for _ in range(len(queue)):
+                current = queue.popleft()
+                for i in range(1, 7):
+                    nxt = current + i
+                    if nxt > goal:
+                        continue
+                    row, col = self.numToCoordinates(nxt, n)
+                    dest = nxt if board[row][col] == -1 else board[row][col]
+                    if dest == goal:
+                        return rolls
+                    if visited[dest - 1]:
+                        continue
+
+                    queue.append(dest)
+                    visited[dest - 1] = True
+        return -1
 
 
 s = Solution()
-s.numToCoordinates(13, 4)
-s.snakesAndLadders(
-    [
-        [-1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1],
-        [-1, 35, -1, -1, 13, -1],
-        [-1, -1, -1, -1, -1, -1],
-        [-1, 15, -1, -1, -1, -1],
-    ]
+print(
+    s.snakesAndLadders(
+        [
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, 35, -1, -1, 13, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, 15, -1, -1, -1, -1],
+        ]
+    )
 )
 
 
