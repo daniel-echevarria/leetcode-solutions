6cf3d043b7c80f4fee5f89f8dafcc3f60febe9e3
Kasper Bak <dan.echevarri@gmail.com>
2025-09-18 17:26:00 +0200

    Add problem 208 with solution

--- python/208-implement-trie.py
+++ python/208-implement-trie.py
@@ -5,24 +5,29 @@ class TrieNode:
 
 
 class Trie:
-
     def __init__(self):
-        self.words = {}
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        self.words[word] = "cool"
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
 
     def search(self, word: str) -> bool:
-        if self.words.get(word):
-            return True
-        return False
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
 
     def startsWith(self, prefix: str) -> bool:
-        return len([word for word in self.words if word.startswith(prefix)]) > 0
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
