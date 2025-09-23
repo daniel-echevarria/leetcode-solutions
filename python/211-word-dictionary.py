5ca1c26f04deaa8d97dbdfbe4a21bd0e5fd3378d
Kasper Bak <dan.echevarri@gmail.com>
2025-09-23 20:45:43 +0200

    Make confusing progress

--- python/211-word-dictionary.py
+++ python/211-word-dictionary.py
@@ -1,16 +1,47 @@
+class Node:
+    def __init__(self):
+        self.is_end = False
+        self.children = {}
+
+
 class WordDictionary:
 
     def __init__(self):
-
+        self.root = Node()
 
     def addWord(self, word: str) -> None:
-
+        node = self.root
+        for char in enumerate(word):
+            if char in node.children:
+                node = node.children[char]
+                continue
+            node.children[char] = Node()
+            node = node.children[char]
+        node.is_end = True
 
     def search(self, word: str) -> bool:
+        node = self.root
+        jumped = []
+        for char in word:
+            if char == "." and not jumped:
+                for values in node.children.values():
+                    jumped.append(values.children)
+
+            else:
+                if char not in node.children:
+                    return False
+                node = node.children[char]
+            jumped = False
+        return node.is_end
 
 
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+wordDictionary = WordDictionary()
+print(
+    wordDictionary.addWord("bad"),
+    wordDictionary.addWord("dad"),
+    wordDictionary.addWord("mad"),
+    wordDictionary.search("pad"),
+    wordDictionary.search("bad"),
+    wordDictionary.search(".ad"),
+    wordDictionary.search("b.."),
+)
