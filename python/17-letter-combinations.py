3f7511f7f706c4bf53061d28ddf2a25fecb1b852
Kasper Bak <dan.echevarri@gmail.com>
2025-09-29 15:14:06 +0200

    Add solution

--- python/17-letter-combinations.py
+++ python/17-letter-combinations.py
@@ -1,28 +1,26 @@
 digits_to_letters = {
-    2: ["a", "b", "c"],
-    3: ["d", "e", "f"],
-    4: ["g", "h", "i"],
-    5: ["j", "k", "l"],
-    6: ["m", "n", "o"],
-    7: ["p", "q", "r", "s"],
-    8: ["t", "u", "v"],
-    9: ["w", "x", "y", "z"],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
 }
+
+results = set()
 
 
 class Solution:
-    def letterCombinations(self, digits: str) -> list[str]:
-        n = len(digits)
-        if n == 1:
-            return digits_to_letters[digits]
-        keys = list(digits)
-        results = set([])
-        i = 0
-        for letter in keys[i]:
-            for let in keys[i - 1]:
-                results.add("".join([letter, let]))
-        print(results)
+    def letterCombinations(self, digits: str, path: str = "", index=0) -> list[str]:
+        if len(path) == len(digits):
+            results.add(path)
+            return
+        for letter in digits_to_letters[digits[index]]:
+            self.letterCombinations(digits, path + letter, index + 1)
 
 
 s = Solution()
-s.letterCombinations("23")
+s.letterCombinations("2345")
+print(results)
