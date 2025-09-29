363961ff5a67ab57cff5726b9489bca33773a27f
Kasper Bak <dan.echevarri@gmail.com>
2025-09-29 19:13:58 +0200

    Add solution

--- python/17-letter-combinations.py
+++ python/17-letter-combinations.py
@@ -1,26 +1,24 @@
-digits_to_letters = {
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"],
-}
-
-results = set()
-
-
 class Solution:
-    def letterCombinations(self, digits: str, path: str = "", index=0) -> list[str]:
+    def __init__(self):
+        self.digits_to_letters = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        self.results = set()
+
+    def helper(self, digits, path="", index=0):
         if len(path) == len(digits):
-            results.add(path)
+            path and self.results.add(path)
             return
-        for letter in digits_to_letters[digits[index]]:
-            self.letterCombinations(digits, path + letter, index + 1)
+        for letter in self.digits_to_letters[digits[index]]:
+            self.helper(digits, path + letter, index + 1)
 
-
-s = Solution()
-s.letterCombinations("2345")
-print(results)
+    def letterCombinations(self, digits: str) -> list[str]:
+        self.helper(digits)
+        return list(self.results)
