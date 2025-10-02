729264e10c383392043734beebb08365fb5f4530
Kasper Bak <dan.echevarri@gmail.com>
2025-10-02 11:13:34 +0200

    change strategy

--- python/77-combinations.py
+++ python/77-combinations.py
@@ -1,2 +1,14 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
+    def __init__(self):
+        self.results = []
+        self.visited = set()
+
+    def helper(self, n, k, path=""):
+        if len(path) == k:
+            standardized = list(path).sort()
+            self.results.append(standardized)
+            return
+
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        for i in range(n):
+            self.helper(n, k, path=str(i + 1))
