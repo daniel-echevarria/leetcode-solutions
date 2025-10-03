247e3df266968c9230cbc195f59966e3aff10b59
Kasper Bak <dan.echevarri@gmail.com>
2025-10-03 10:46:38 +0200

    Implement first step (all possibilities)

--- python/77-combinations.py
+++ python/77-combinations.py
@@ -5,10 +5,17 @@ class Solution:
 
     def helper(self, n, k, path=""):
         if len(path) == k:
-            standardized = list(path).sort()
+            standardized = sorted(list(path))
             self.results.append(standardized)
             return
+        for i in range(n):
+            self.helper(n, k, path + str(i + 1))
 
     def combine(self, n: int, k: int) -> list[list[int]]:
         for i in range(n):
             self.helper(n, k, path=str(i + 1))
+        return self.results
+
+
+s = Solution()
+print(s.combine(4, 2))
