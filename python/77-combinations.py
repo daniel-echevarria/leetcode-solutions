a10bbdc1f0bc164cff03c57072b8c8700ac6d5a6
Kasper Bak <dan.echevarri@gmail.com>
2025-10-05 10:41:02 +0200

    Add working solution

--- python/77-combinations.py
+++ python/77-combinations.py
@@ -1,24 +1,17 @@
 class Solution:
     def __init__(self):
         self.results = []
-        self.visited = set()
 
-    def helper(self, n, k, path=""):
+    def helper(self, n, k, path):
         if len(path) == k:
-            standardized = sorted(list(path))
-            standardized[:] = map(lambda x: int(x), standardized)
-            self.results.append(standardized)
+            self.results.append(path)
             return
         for i in range(n + 1):
-            if str(i + 1) in path:
+            if i + 1 in path:
                 return
-            self.helper(n, k, path + str(i + 1))
+            self.helper(n, k, [*path, i + 1])
 
     def combine(self, n: int, k: int) -> list[list[int]]:
         for i in range(n):
-            self.helper(n, k, path=str(i + 1))
-        return sorted(self.results)
-
-
-s = Solution()
-print(s.combine(4, 2))
+            self.helper(n, k, path=[i + 1])
+        return self.results
