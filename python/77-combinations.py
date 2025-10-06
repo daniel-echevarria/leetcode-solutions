c60c175210db2b0665627cc3ae2139257cfbb89d
Kasper Bak <dan.echevarri@gmail.com>
2025-10-06 08:54:25 +0200

    Find optimal solution

--- python/77-combinations.py
+++ python/77-combinations.py
@@ -2,16 +2,17 @@ class Solution:
     def __init__(self):
         self.results = []
 
-    def helper(self, n, k, path):
+    def helper(self, n, k, path=[], current=0):
         if len(path) == k:
             self.results.append(path)
             return
-        for i in range(n + 1):
-            if i + 1 in path:
-                return
-            self.helper(n, k, [*path, i + 1])
+        for i in range(current, n):
+            self.helper(n, k, [*path, i + 1], i + 1)
 
     def combine(self, n: int, k: int) -> list[list[int]]:
-        for i in range(n):
-            self.helper(n, k, path=[i + 1])
+        self.helper(n, k)
         return self.results
+
+
+s = Solution()
+print(s.combine(4, 2))
