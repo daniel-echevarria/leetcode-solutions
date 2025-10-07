82e50538a51761e26b7432c006f615e117b85127
Kasper Bak <dan.echevarri@gmail.com>
2025-10-07 09:35:04 +0200

    Refactor into one function

--- python/77-combinations.py
+++ python/77-combinations.py
@@ -1,17 +1,17 @@
 class Solution:
-    def __init__(self):
-        self.results = []
-
-    def helper(self, n, k, path=[], current=0):
-        if len(path) == k:
-            self.results.append(path)
-            return
-        for i in range(current, n):
-            self.helper(n, k, [*path, i + 1], i + 1)
 
     def combine(self, n: int, k: int) -> list[list[int]]:
-        self.helper(n, k)
-        return self.results
+        results = []
+
+        def backtrack(n, k, path=[], current=0):
+            if len(path) == k:
+                results.append(path)
+                return
+            for i in range(current, n):
+                backtrack(n, k, [*path, i + 1], i + 1)
+
+        backtrack(n, k)
+        return results
 
 
 s = Solution()
