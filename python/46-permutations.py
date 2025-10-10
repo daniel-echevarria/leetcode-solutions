c319da162d919d9c73390c319196cd046691236f
Kasper Bak <dan.echevarri@gmail.com>
2025-10-10 15:15:50 +0200

    Failed attempt

--- python/46-permutations.py
+++ python/46-permutations.py
@@ -1,2 +1,25 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
+        results = []
+        stored = set()
+
+        def helper(i):
+            option = nums[:]
+            for j in range(n):
+                option[j] = nums[i]
+                option[i] = nums[j]
+                string_version = "".join(list(map(str, option)))
+                if string_version not in stored:
+                    results.append(option)
+                stored.add(string_version)
+                option = nums[:]
+
+        for i in range(n):
+            helper(i)
+        print(results)
+        return results
+
+
+s = Solution()
+s.permute([1, 2, 3])
