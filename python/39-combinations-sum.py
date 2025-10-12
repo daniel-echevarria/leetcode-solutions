993842fd80d257df187a02b4293741229bb4c4ff
Kasper Bak <dan.echevarri@gmail.com>
2025-10-12 09:08:46 +0200

    Implement solution

--- python/39-combinations-sum.py
+++ python/39-combinations-sum.py
@@ -1,2 +1,22 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        result = []
+
+        def backtrack(start, path, moves, total):
+            if total == target:
+                result.append(path[:])
+                return
+            if total > target:
+                return
+            for i in range(start, len(moves)):
+                backtrack(
+                    i, path=[*path, moves[i]], moves=moves, total=total + moves[i]
+                )
+
+        backtrack(0, [], candidates, 0)
+        print(result)
+        return result
+
+
+s = Solution()
+s.combinationSum([2, 3, 5], 8)
