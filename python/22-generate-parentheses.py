92d59d66ca0d71a4131882bb61ea700390c38334
Kasper Bak <dan.echevarri@gmail.com>
2025-10-14 12:40:54 +0200

    Make progress

--- python/22-generate-parentheses.py
+++ python/22-generate-parentheses.py
@@ -1,4 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
         results = []
-        def backtrack(path, moves):
+        count = {"(": 0, ")": 0}
+
+        def backtrack(path, current):
+            count[current] += 1
+            if len(path) == n * 2:
+                results.append(path)
+                return
+            if count[")"] < count["("]:
+                backtrack(path + ")", current=")")
+            if path.count("(") < n:
+                print(path)
+                backtrack(path + "(", current="(")
+
+        backtrack("(", "(")
+        print(results)
+
+
+s = Solution()
+s.generateParenthesis(3)
