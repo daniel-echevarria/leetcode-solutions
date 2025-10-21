61db59ccef587261be402cf59c8275bab2338ed4
Kasper Bak <dan.echevarri@gmail.com>
2025-10-21 13:49:58 +0200

    Attempt with board

--- python/52-total-n-queens.py
+++ python/52-total-n-queens.py
@@ -1,2 +1,35 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
+        board = [[0] * n for _ in range(n)]
+
+        solutions = set()
+
+        def backtrack(y, x, path="", i=[], j=[]):
+            if len(path) == n * 3:
+                solutions.add(path)
+                return
+            board[y][x] = "#"
+            i.append(y)
+            j.append(x)
+            for k in range(n):
+                for l in range(n):
+                    if k in i or l in j:
+                        continue
+                    backtrack(k, l, path + f"{y},{x}", i, j)
+
+        for y in range(n):
+            for x in range(n):
+                backtrack(y, x)
+
+
+s = Solution()
+print(s.totalNQueens(4))
+
+# Algo
+# Declare a valid path array
+# iterate through all the squares in the the n * n matrix
+# For each square attempt to make a placement from there
+# recursing with the possible squares left and the current path
+# when the length of the path is equal to n
+# add the path to the valid path
+#
