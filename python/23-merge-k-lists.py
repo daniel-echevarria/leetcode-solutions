02b272821ca7d66a5f02c34e106151c9dcaf9f7c
Kasper Bak <dan.echevarri@gmail.com>
2025-10-23 11:24:32 +0200

    Improve solution

--- python/23-merge-k-lists.py
+++ python/23-merge-k-lists.py
@@ -2,40 +2,37 @@ from collections import deque
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
     def mergeKLists(self, lists):
-        dummy = ListNode(float("-inf"))
         if not lists:
             return
 
-        def merge(l1, l2, current):
-            if not l1:
-                current.next = l2
-                return dummy
-            if not l2:
-                current.next = l1
-                return dummy
-            if l1.val <= l2.val:
-                current.next = ListNode(l1.val)
-                current = current.next
-                l1 = l1.next
+        def merge(l1, l2):
+            if not l1 or not l2:
+                return l1 or l2
+            if l1.val < l2.val:
+                l1.next = merge(l1.next, l2)
+                return l1
             else:
-                current.next = ListNode(l2.val)
-                current = current.next
-                l2 = l2.next
-            return merge(l1, l2, current)
+                l2.next = merge(l2.next, l1)
+                return l2
 
         while len(lists) > 1:
-            l1 = lists.pop()
-            l2 = lists.pop()
-            res = merge(l1, l2, dummy)
-            lists.append(res.next)
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = None
+                if i + 1 < len(lists):
+                    l2 = lists[i + 1]
+                res = merge(l1, l2)
+                merged.append(res)
+            lists = merged
 
         return lists[0]
 
