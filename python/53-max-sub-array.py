1f95be890e8735184d942592d5fef4af9bb49e7a
Kasper Bak <dan.echevarri@gmail.com>
2025-10-25 15:13:39 +0200

    Add divide and conquer solution

--- python/53-max-sub-array.py
+++ python/53-max-sub-array.py
@@ -1,8 +1,28 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        max_sum = current_sum = nums[0]
+        def helper(l, r):
+            if l == r:
+                return nums[l]
+            mid = (l + r) // 2
 
-        for n in nums[1:]:
-            current_sum = max(n, current_sum + n)
-            max_sum = max(current_sum, max_sum)
-        return max_sum
+            left_sum = helper(l, mid)
+            right_sum = helper(mid + 1, r)
+
+            cross_left = float("-inf")
+            ls = 0
+            for i in range(mid, l - 1, -1):
+                ls += nums[i]
+                cross_left = max(cross_left, ls)
+
+            cross_right = float("-inf")
+            rs = 0
+            for i in range(mid + 1, r + 1):
+                rs += nums[i]
+                cross_right = max(cross_right, rs)
+
+            sum_cross = cross_left + cross_right
+            res = max(left_sum, right_sum, sum_cross)
+
+            return res
+
+        return helper(0, len(nums) - 1)
