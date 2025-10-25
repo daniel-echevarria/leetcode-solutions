f9706a35fa6490dc39fd6b980d12de329f4d98b9
Kasper Bak <dan.echevarri@gmail.com>
2025-10-25 14:27:03 +0200

    Implement solution

--- python/53-max-sub-array.py
+++ python/53-max-sub-array.py
@@ -1,2 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
+        max_sum = current_sum = nums[0]
+
+        for n in nums[1:]:
+            current_sum = max(n, current_sum + n)
+            max_sum = max(current_sum, max_sum)
+        return max_sum
