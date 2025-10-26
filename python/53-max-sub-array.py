b792331a9891ea8c41cf4f42b07c43ba8c203a95
Kasper Bak <dan.echevarri@gmail.com>
2025-10-26 11:36:33 +0100

    Refactor: back to divide and conquer

--- python/53-max-sub-array.py
+++ python/53-max-sub-array.py
@@ -1,12 +1,26 @@
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        current_sum = max_sum = nums[0]
-        for i in range(1, len(nums)):
-            current_sum = max(n, current_sum + nums[i])
-            max_sum = max(max_sum, current_sum)
-        return max_sum
+        def helper(l, r):
+            if l == r:
+                return nums[l]
+            mid = (l + r) // 2
 
+            left_sum = helper(l, mid)
+            right_sum = helper(mid + 1, r)
 
-s = Solution()
-nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(s.maxSubArray(nums))
+            cross_left = float("-inf")
+            l_sum = 0
+            for i in range(mid, l - 1, -1):
+                l_sum += nums[i]
+                cross_left = max(cross_left, l_sum)
+
+            cross_right = float("-inf")
+            r_sum = 0
+            for i in range(mid + 1, r + 1):
+                r_sum += nums[i]
+                cross_right = max(cross_right, r_sum)
+            cross_sum = cross_left + cross_right
+            res = max(right_sum, left_sum, cross_sum)
+            return res
+
+        return helper(0, len(nums) - 1)
