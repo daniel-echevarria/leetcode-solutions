8e959a3c5030954e54ce42e6669973d4d71e9956
Kasper Bak <dan.echevarri@gmail.com>
2025-10-31 11:29:09 +0100

    Change file name and add solution

--- python/215-find-kth-largest.py
+++ python/215-find-kth-largest.py
@@ -1,2 +1,18 @@
+import heapq
+
+
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        max_heap = [-n for n in nums]
+        heapq.heapify(max_heap)
+        for _ in range(k):
+            biggest = heapq.heappop(max_heap)
+        return -biggest
+
+
+s = Solution()
+# nums = [3, 2, 1, 5, 6, 4]
+# k = 2
+nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+k = 4
+print(s.findKthLargest(nums, k))
