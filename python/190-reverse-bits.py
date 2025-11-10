d6f5868afdcca601b47adf3babc785e1eef4c3e1
Kasper Bak <dan.echevarri@gmail.com>
2025-11-10 16:56:48 +0100

    Add bit manipulation solution

--- python/190-reverse-bits.py
+++ python/190-reverse-bits.py
@@ -1,2 +1,13 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        start = 0
+        for _ in range(32):
+            bit = n & 1
+            start = (start << 1) | bit
+            n >>= 1
+        return start
+
+
+s = Solution()
+n = 43261596
+s.reverseBits(n)
