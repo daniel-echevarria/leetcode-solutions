f5c201b42c3580b08f7d646e3fbcb9b89ea0bfc0
Kasper Bak <dan.echevarri@gmail.com>
2025-11-12 13:28:01 +0100

    Implement killer solution

--- python/66-plus-one.py
+++ python/66-plus-one.py
@@ -1,2 +1,17 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        r = len(digits) - 1
+        carry = 1
+        while carry:
+            if r < 0:
+                return [1] + digits
+            digits[r] += 1
+            if digits[r] < 10:
+                return digits
+            digits[r] = 0
+            r -= 1
+
+
+s = Solution()
+digits = [9]
+print(s.plusOne(digits))
