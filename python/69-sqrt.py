403b4ede6a0a11fe4c24f7a366784502fcdc7309
Kasper Bak <dan.echevarri@gmail.com>
2025-11-14 09:48:06 +0100

    Implement optimal solution

--- python/69-sqrt.py
+++ python/69-sqrt.py
@@ -1,27 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        l, r = 0, x
+        if x < 2:
+            return x
+
+        l, r = 0, x // 2
+        ans = 1
+
         while l <= r:
             mid = (l + r) // 2
-            square = mid * mid
-            next_square = (mid + 1) * (mid + 1)
-            if square <= x and next_square > x:
+            sq = mid * mid
+            if sq == x:
                 return mid
-            elif square >= x:
-                r = mid
-            else:
+            elif sq < x:
+                ans = mid
                 l = mid + 1
-
-
-s = Solution()
-print(s.mySqrt(1))
-
-# Algo
-# Declare two pointers l and r with initial value of 0 and of x
-# Loop until l is bigger than r
-# Each round check the middle value
-# Compare it's square to x
-# If it's bigger recurse the left half
-# If it's smaller recurse the right half
+            else:
+                r = mid - 1
+        return ans
