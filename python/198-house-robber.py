2b6e4222b7cd06dbb8051c1f06173e7c0ee47887
Kasper Bak <dan.echevarri@gmail.com>
2025-11-16 10:51:04 +0100

    Add efficient solution

--- python/198-house-robber.py
+++ python/198-house-robber.py
@@ -1,19 +1,24 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        even_sum = 0
-        odd_sum = 0
-        for i in range(len(nums)):
-            money = nums[i]
-            if i % 2 == 0:
-                even_sum += money
-            else:
-                odd_sum += money
-        return max(even_sum, odd_sum)
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        elif n == 2:
+            return max(nums[0], nums[1])
+        money = {}
+        money[0] = nums[0]
+        money[1] = nums[1]
+        money[2] = nums[2] + nums[0]
+        max_money = max(money[0], money[1], money[2])
+        for i in range(3, n):
+            money[i] = nums[i] + max(money[i - 2], money[i - 3])
+            max_money = max(money[i], max_money)
+        return max_money
 
 
 s = Solution()
-nums = [1, 2, 3, 1]
+# nums = [1, 2, 3, 1]
+# nums = [9, 1, 1, 9]
+nums = [2, 7, 9, 3, 1]
+
 print(s.rob(nums))
-# Algo sum the values at even intervals
-# sum the values at odd intervals
-# return the biggest sum
