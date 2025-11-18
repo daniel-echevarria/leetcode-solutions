40573fd9e4880747c865f5e7c35ac827f5229e3d
Kasper Bak <dan.echevarri@gmail.com>
2025-11-18 10:39:01 +0100

    Add optimal solution

--- python/139-word-break.py
+++ python/139-word-break.py
@@ -1,7 +1,38 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        memo = {}
+
+        def dfs(i):
+            if i == len(s):
+                return True
+            if i in memo:
+                return memo[i]
+
+            for word in wordDict:
+                if s.startswith(word, i):
+                    if dfs(i + len(word)):
+                        memo[i] = True
+                        return True
+
+            memo[i] = False
+            return False
+
+        return dfs(0)
+
 
 sol = Solution()
 s = "leetcode"
-wordDict = ["leet","code"]
+wordDict = ["leet", "code"]
+# s = "cars"
+# wordDict = ["car", "ca", "rs"]
+# s = "ddadddbdddadd"
+# wordDict = ["dd", "ad", "da", "b"]
+# s = "catsandog"
+# wordDict = ["cats", "dog", "sand", "and", "cat"]
 print(sol.wordBreak(s, wordDict))
+
+
+# Brute force:
+# Iterate through the wordDict array, if the word is a substring of s
+# recursively break the word. If current string  is empty return true
+# after iteration return false
