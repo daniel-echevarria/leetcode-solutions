4d2f3bf759605de219de58513fb70ecc60726f9a
Kasper Bak <dan.echevarri@gmail.com>
2025-11-21 10:12:49 +0100

    Add: DP solution

--- python/300-lengt-of-lis.py
+++ python/300-lengt-of-lis.py
@@ -1,2 +1,16 @@
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        n = len(nums)
+        memo = [1] * (n + 1)
+
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    memo[i] = max(memo[i], memo[j] + 1)
+
+        return max(memo)
+
+
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+s = Solution()
+print(s.lengthOfLIS(nums))
