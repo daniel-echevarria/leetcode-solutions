f813a6460000fb62f76e9f1c4f8921a7ed5db512
Kasper Bak <dan.echevarri@gmail.com>
2025-11-21 10:13:06 +0100

    Implement solution

--- python/322-coin-change.py
+++ python/322-coin-change.py
@@ -1,31 +1,47 @@
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
-        memo = [float("inf")]
+        if amount == 0:
+            return 0
 
-        coins.sort(reverse=True)
-        print(coins)
+        memo = [False] * (amount + 1)
 
-        def dfs(num, count=0):
-            if num == 0:
-                memo[num] = min(memo[num], count)
-                return True
-            if num < 0:
-                return False
+        for coin in coins:
+            if coin > amount:
+                continue
+            memo[coin] = 1
+
+        def dp(num):
+            if memo[num]:
+                return memo[num]
+            min_steps = []
             for coin in coins:
-                if dfs(num - coin, count + 1):
-                    return
+                if num - coin > 0 and memo[num - coin]:
+                    min_steps.append(memo[num - coin])
+            if min_steps:
+                return min(min_steps) + 1
+            return False
 
-        dfs(amount)
-        return -1 if memo[0] == float("inf") else memo[0]
+        for i in range(1, amount + 1):
+            memo[i] = dp(i)
+
+        return memo[amount] or -1
 
 
-s = Solution()
 coins = [1, 2, 5]
 amount = 11
-# coins = [2]
-# amount = 3
+# coins = [3]
+# amount = 4
+s = Solution()
 print(s.coinChange(coins, amount))
 
-# Algo, given a number
-# Declare a variable memo with an initial value of an empty dict
-# Recursively iterate through the coins
+
+# Algorithm:
+# Declare a memo of size amount with each value being False
+# Then add the base cases by iterating through the coins and giving
+# That key in the memo a value of 1
+# Then iterate from 1 to the amount
+# If the current value is in the memo return that
+# Otherwise check for each possible coins we came what is the minimum
+# we could have and add 1
+# if there was no coin that could reach it justs stays false
+# return memo[amount]
