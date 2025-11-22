e1b013ddf922d012c6dfa91cb0fde0c5b4471b42
Kasper Bak <dan.echevarri@gmail.com>
2025-11-22 21:21:46 +0100

    Implement solution

--- python/63-unique-path-2.py
+++ python/63-unique-path-2.py
@@ -1,2 +1,26 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[i])):
+                if obstacleGrid[i][j] == 1:
+                    obstacleGrid[i][j] = 0
+                elif i == 0 and j == 0:
+                    obstacleGrid[i][j] = 1
+                elif i == 0:
+                    obstacleGrid[i][j] += obstacleGrid[i][j - 1]
+                elif j == 0:
+                    obstacleGrid[i][j] += obstacleGrid[i - 1][j]
+                else:
+                    obstacleGrid[i][j] += (
+                        obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+                    )
+        return obstacleGrid[-1][-1]
+
+
+s = Solution()
+obstacleGrid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+print(s.uniquePathsWithObstacles(obstacleGrid))
+
+
+# Algo, for each square if the square is 1, continue otherwise it becomes the sum
+# of the top and left square if they exist
