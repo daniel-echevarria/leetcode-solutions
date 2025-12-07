4732f2a12dae059dfe0585b309b1f1cc31d86a67
Kasper Bak <dan.echevarri@gmail.com>
2025-12-07 14:37:52 +0100

    Add personal solution (senior style)

--- python/q3-exclusive-time.py
+++ python/q3-exclusive-time.py
@@ -3,52 +3,36 @@ from collections import defaultdict
 
 class Solution:
     def exclusiveTime(self, n: int, logs: list[str]) -> list[int]:
-        times = defaultdict(int)
+        times = [0] * n
         stack = []
-        prev_depth = 0
-        prev_time = 0
-        depth = 0
 
         for l in logs:
-            fn, action, ts = l.split(":")
+            fn, a, ts = l.split(":")
             fn = int(fn)
             ts = int(ts)
-            if (
-                action == "end"
-                and stack
-                and stack[-1][1] == "start"
-                and stack[-1][0] == fn
-            ):
-                _, _, pts = stack.pop()
-                total_time = ts - pts + 1
-                if depth > prev_depth:
-                    times[fn] += total_time - prev_time
-                    prev_time = total_time
-                else:
-                    prev_time += total_time
+            if a == "end" and stack[-1][1] == "start" and stack[-1][0] == fn:
+                prev = stack.pop()
+                total_time = ts - prev[2] + 1
+                specific_time = total_time - prev[3]
+                times[fn] += specific_time
+                if stack:
+                    stack[-1][3] += total_time
             else:
-                stack.append((fn, action, ts))
-                depth += 1
-                prev_depth += 1
+                stack.append([fn, a, ts, 0])
 
-        return [times[i] for i in range(n)]
+        return times
 
-
-# logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
-logs = ["0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7"]
-n = 2
-
-s = Solution()
-print(s.exclusiveTime(n, logs))
 
 # Algo
-# Declare  an empty map times
-# Declare an empty stack
-# Iterate through the logs
-# If the current log is an end, and the previous log is the start of
-# that function, pop the start and add the time difference
-# to the times map with the key of the function
-# otherwise just push a workable version (tuple) of
-# the log to the stack
-# In the end iterate in the range n providing the sum from
-# times
+# iterate through the logs
+# if the current log closes the previous
+# pop the previous and add the difference to the map - the inner_time_value
+# and update the inner_time value
+# Keep on going until all the logs where seen
+
+
+# logs = ["0:start:0", "0:start:2", "0:end:5", "1:start:7", "1:end:7", "0:end:8"]
+logs = ["0:start:0", "0:start:1", "0:start:2", "0:end:3", "0:end:4", "0:end:5"]
+n = 2
+s = Solution()
+print(s.exclusiveTime(n, logs))
