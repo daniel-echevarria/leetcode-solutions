7f4fa889f1f7b67836289070dd0901ea040390d2
Kasper Bak <dan.echevarri@gmail.com>
2025-12-19 13:24:27 +0100

    bang head against wall

--- python/construct-target-array-with-multiple-sums.py
+++ python/construct-target-array-with-multiple-sums.py
@@ -1,30 +1,32 @@
+import heapq
+
+
 class Solution:
     def isPossible(self, target: list[int]) -> bool:
-        n = len(target)
-        target.sort()
-        print(target)
-        found_target = False
-        start = [1] * n
+        heap = [-m for m in target]
+        heapq.heapify(heap)
+        while True:
+            biggest = heapq.heappop(heap)
+            rest = sum(heap)
 
-        def helper(arr):
-            nonlocal found_target
-            if arr == target:
-                found_target = True
-                return
-            c_sum = sum(arr)
-            if c_sum > target[-1]:
+            if -biggest == 1:
+                return True
+
+            if rest == 0 or biggest > rest:
                 return False
-            for i in range(len(arr)):
-                copy = arr.copy()
-                copy[i] = c_sum
-                helper(copy)
 
-        helper(start)
-        return found_target
+            replaced = biggest % rest
+            heapq.heappush(heap, replaced)
+
+            if replaced > -1:
+                return False
 
 
 s = Solution()
 target = [9, 3, 5]
+# target = [1, 1000000000]
+# target = [1, 1, 1, 2]
+
 print(s.isPossible(target))
 # Algo
 # Notes:
@@ -37,3 +39,9 @@ print(s.isPossible(target))
 # with the sum recursively.
 # if at any point the sum is bigger than the smallest number we return false
 # if at some point the array is like the target we change isPossible
+
+# Second:
+# Heapify the target to get the biggest value
+# pop it, compute what it replaced and append back to the heap
+# if at some point it's less than 1 return False
+# if at some point it equals the start return true
