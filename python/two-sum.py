335eba3790e1d3f72e3727fa1a9494415f69271e
Kasper Bak <dan.echevarri@gmail.com>
2025-12-28 11:25:35 +0100

    Add problem with alright solution

---
+++ python/two-sum.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        indices = defaultdict(list)
+
+        for i, n in enumerate(nums):
+            indices[n].append(i)
+
+        nums.sort()
+        l, r = 0, len(nums) - 1
+        while l < r:
+            current_sum = nums[l] + nums[r]
+            if current_sum == target:
+                return [indices[nums[l]].pop(), indices[nums[r]].pop()]
+            if current_sum < target:
+                l += 1
+            if current_sum > target:
+                r -= 1
+
+
+s = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+print(s.twoSum(nums, target))
+
+# Algo
+# Sort the array
+# use two pointers l and r
+# launch a loop that runs until the sum of the values at l and r
+# equal the target or that l == r
+# return l an r
