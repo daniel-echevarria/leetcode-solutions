59abd771897dac37dcff0734cebfd8df5fd53e06
Kasper Bak <dan.echevarri@gmail.com>
2026-01-04 14:29:04 +0100

    Add problem with optimal solution

---
+++ python/1413-min-start-value.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minStartValue(self, nums: list[int]) -> int:
+        deepest = float("inf")
+        current = 0
+        for n in nums:
+            current += n
+            deepest = min(deepest, current)
+
+        if deepest < 1:
+            return abs(deepest) + 1
+        return 1
+
+
+s = Solution()
+# nums = [-3, 2, -3, 4, 2]
+# nums = [1, 2]
+nums = [1, -2, -3]
+print(s.minStartValue(nums))
+
+# Algo:
+# Iterate through the nums while keeping track of the current total
+# Track the min current total overall
+# return the absolute value of min + 1 if the min current is below 1
+# otherwise return 1
