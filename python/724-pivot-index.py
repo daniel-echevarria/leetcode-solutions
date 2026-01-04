e41f34683c9c91fd5db41ab55bc98c0a977cf98a
Kasper Bak <dan.echevarri@gmail.com>
2026-01-04 14:04:40 +0100

    Add problem 724 with solution

---
+++ python/724-pivot-index.py
@@ -0,0 +1,25 @@
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        prefix = [nums[0]]
+        for i in range(1, len(nums)):
+            prefix.append(nums[i] + prefix[i - 1])
+
+        for i, n in enumerate(prefix):
+            left_sum = prefix[i - 1] if i > 0 else 0
+            right_sum = prefix[-1] - prefix[i]
+            if left_sum == right_sum:
+                return i
+        return -1
+
+
+s = Solution()
+# nums = [1, 7, 3, 6, 5, 6]
+# nums = [2, 1, -1]
+nums = [3, -3, 5]
+print(s.pivotIndex(nums))
+
+# Make a prefix sum array
+# Iterate through the prefix array
+# if at any point the previous value
+# is the same as the last prefix sum minus the current value
+# return the index
