8b2508d5a56e38a50a4fdeb2db17652ea21b2a40
Kasper Bak <dan.echevarri@gmail.com>
2026-01-06 22:05:44 +0100

    Add problem

---
+++ python/1588-sum-odd-length.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: list[int]) -> int:
+
+# Algo
+# Getting the total of the single value arrays is trivial
+# so we will loop until the right pointer is equal to the length of the original array
+# each time
+
