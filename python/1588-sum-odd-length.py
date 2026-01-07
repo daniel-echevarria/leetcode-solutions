1b72fa197f7593ff0023bba9ac29d58d73f4ca6c
Kasper Bak <dan.echevarri@gmail.com>
2026-01-07 10:21:54 +0100

    Add brute force solution

--- python/1588-sum-odd-length.py
+++ python/1588-sum-odd-length.py
@@ -1,8 +1,31 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: list[int]) -> int:
+        n = sub_len = len(arr)
+        total = sum(arr)
+        while sub_len > 2:
+            if sub_len % 2 == 0:
+                sub_len -= 1
+                continue
+            l, r = 0, sub_len
+            while r <= n:
+                total_sub = sum(arr[l:r])
+                total += total_sub
+                l += 1
+                r += 1
+            sub_len -= 2
+        return total
+
+
+s = Solution()
+arr = [1, 4, 2, 5, 3]
+print(s.sumOddLengthSubarrays(arr))
+
 
 # Algo
-# Getting the total of the single value arrays is trivial
-# so we will loop until the right pointer is equal to the length of the original array
-# each time
-
+# declare a variable n that get the length of the array
+# declare a variable total that gets 0
+# loop until n is 0
+# if n is even continue
+# otherwise traverse the array accumulating the sum of the arrays
+# of length n in the total
+# return the total
