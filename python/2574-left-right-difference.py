4ce645ceb3b44dcb4e2672875140040767228216
Kasper Bak <dan.echevarri@gmail.com>
2026-01-11 18:39:52 +0100

    Add solution

--- python/2574-left-right-difference.py
+++ python/2574-left-right-difference.py
@@ -1,2 +1,24 @@
 class Solution:
-    def leftRightDifference(self, nums: List[int]) -> List[int]:
+    def leftRightDifference(self, nums: list[int]) -> list[int]:
+        prefix = []
+        curr = 0
+        for n in nums:
+            curr += n
+            prefix.append(curr)
+        curr = 0
+        answer = []
+        for i in range(len(nums) - 1, -1, -1):
+            curr += nums[i]
+            answer.append(abs(prefix[i] - curr))
+        answer.reverse()
+        return answer
+
+
+s = Solution()
+nums = [10, 4, 8, 3]
+print(s.leftRightDifference(nums))
+
+
+# Algo
+# create both prefix sums
+# return the difference
