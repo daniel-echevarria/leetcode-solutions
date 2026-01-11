089fb7f0b2e30e9678c957e0d8bf09ed65a08b6f
Kasper Bak <dan.echevarri@gmail.com>
2026-01-11 17:42:29 +0100

    Add solution

--- python/2485-find-pivot-integer.py
+++ python/2485-find-pivot-integer.py
@@ -1,2 +1,25 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
+        arr = []
+        for i in range(1, n + 1):
+            arr.append(i)
+
+        total = sum(arr)
+        curr = 0
+        for i in range(n + 1):
+            curr += i
+            if curr == total - curr + i:
+                return i
+        return -1
+
+
+s = Solution()
+print(s.pivotInteger(1))
+
+# Create the array
+# get the total sum
+# iterate while keeping the sum
+# if at some point the sum is the same as the total minus the sum + the current
+# return the index
+# after iteration
+# return the -1
