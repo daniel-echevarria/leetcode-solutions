c5963a135ce5779027a11106ead5cebac2b3f9d3
Kasper Bak <dan.echevarri@gmail.com>
2026-01-15 17:58:42 +0100

    Add optimal solution

--- python/3427-sum-varialb-sub.py
+++ python/3427-sum-varialb-sub.py
@@ -1,10 +1,16 @@
+from itertools import accumulate
+
+
 class Solution:
     def subarraySum(self, nums: list[int]) -> int:
         n = len(nums)
         total = 0
+        acc = list(accumulate(nums))
         for i in range(n):
-            start = max(0, i - nums[i])
-            total += sum(nums[start : i + 1])
+            start = max(0, i - nums[i]) - 1
+            total += acc[i]
+            if start >= 0:
+                total -= acc[start]
         return total
 
 
