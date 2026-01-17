7b5ccecf3831ad190a442724b54db0f946edd808
Kasper Bak <dan.echevarri@gmail.com>
2026-01-17 18:08:45 +0100

    Optimize

--- python/3707-equal-scores-sub.py
+++ python/3707-equal-scores-sub.py
@@ -1,37 +1,13 @@
+from itertools import accumulate
+
+
 class Solution:
     def scoreBalance(self, s: str) -> bool:
-        alpha = [
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "q",
-            "r",
-            "s",
-            "t",
-            "u",
-            "v",
-            "w",
-            "x",
-            "y",
-            "z",
-        ]
-        prefix = []
-        curr = 0
-        for char in s:
-            curr += alpha.index(char) + 1
-            prefix.append(curr)
+        prefix = list(accumulate([ord(char) - 96 for char in s]))
         total = prefix[-1]
         return True if total / 2 in prefix else False
+
+
+st = "adcb"
+s = Solution()
+print(s.scoreBalance(st))
