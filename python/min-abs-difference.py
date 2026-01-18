30129d2eeb8579500c6d23171fd66718ee52fa82
Kasper Bak <dan.echevarri@gmail.com>
2026-01-18 10:41:20 +0100

    Add solution

--- python/min-abs-difference.py
+++ python/min-abs-difference.py
@@ -1,2 +1,27 @@
 class Solution:
     def minimumAbsDifference(self, arr: list[int]) -> list[list[int]]:
+        arr.sort()
+        res = []
+        min_dif = float("inf")
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i - 1]
+            pair = [arr[i - 1], arr[i]]
+            if diff < min_dif:
+                res = [pair]
+                min_dif = diff
+                continue
+            if diff == min_dif:
+                res.append(pair)
+
+        return res
+
+
+s = Solution()
+nums = [4, 2, 1, 3]
+print(s.minimumAbsDifference(nums))
+
+
+# Algo
+# sort the array
+# iterate through the sorted array while keepin track of the smallest difference
+# append to the result and clean if found a smaller difference
