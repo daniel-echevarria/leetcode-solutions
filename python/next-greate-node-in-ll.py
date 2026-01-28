b0217027e84b2b92fabc1acadf42dd9fb0d82886
Kasper Bak <dan.echevarri@gmail.com>
2026-01-28 11:54:01 +0100

    Add algo and start logic

--- python/next-greate-node-in-ll.py
+++ python/next-greate-node-in-ll.py
@@ -5,3 +5,16 @@
 #         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+
+        values = [head.val]
+        next_bigger = [0]
+        current = head.next
+        while current:
+            head = head.next
+
+
+# Bruteforce:
+# Iterate through the ll
+# keep track of the first_non_assigned index
+# the moment the slope goes up, compare all the non assigned indexes from the forst one and update them if the value
+# is smaller than the current value
