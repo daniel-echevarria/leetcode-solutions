92235251ce7aa315c4d7dab146a495265dc6c9a8
Kasper Bak <dan.echevarri@gmail.com>
2026-01-29 15:14:15 +0100

    Add optimal solution

--- python/next-greate-node-in-ll.py
+++ python/next-greate-node-in-ll.py
@@ -5,16 +5,25 @@
 #         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-
-        values = [head.val]
-        next_bigger = [0]
-        current = head.next
-        while current:
+        values = []
+        stack = []
+        res = []
+        idx = 0
+        while head:
+            while stack and values[stack[-1]] < head.val:
+                smaller_idx = stack.pop()
+                res[smaller_idx] = head.val
+            values.append(head.val)
+            stack.append(idx)
+            res.append(0)
             head = head.next
+            idx += 1
+        return res
 
 
-# Bruteforce:
-# Iterate through the ll
-# keep track of the first_non_assigned index
-# the moment the slope goes up, compare all the non assigned indexes from the forst one and update them if the value
-# is smaller than the current value
+# Iterate through the linked list while adding the index to a stack
+# and creating an array of the met values;
+# when adding the indexes, if the stack is empty
+# just continue
+# otherwise pop until the value is equal or bigger that the current value
+# and for each of this popes values add the current value at that index in the answer
