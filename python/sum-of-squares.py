482e089db626f1b77c11e8f5518ac44a815fdbdf
Kasper Bak <dan.echevarri@gmail.com>
2026-01-30 13:20:46 +0100

    Add solution

--- python/sum-of-squares.py
+++ python/sum-of-squares.py
@@ -1,11 +1,19 @@
+import math
+
+
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        l, r = 0, c / 2
-        while l < r:
+        l, r = 0, math.isqrt(c)
+        while l <= r:
             curr = l * l + r * r
             if curr == c:
                 return True
+            if curr > c:
+                r -= 1
+            if curr < c:
+                l += 1
+        return False
 
 
 s = Solution()
-print(s.judgeSquareSum(2))
+print(s.judgeSquareSum(8))
