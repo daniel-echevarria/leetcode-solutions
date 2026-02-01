3e036b0c6a9214a89dab1063c377c6b3bdf46d38
Kasper Bak <dan.echevarri@gmail.com>
2026-02-01 11:04:29 +0100

    Add solution copied from chat gpt

--- python/beautiful-array.py
+++ python/beautiful-array.py
@@ -1,2 +1,21 @@
 class Solution:
-    def beautifulArray(self, n: int) -> List[int]:
+    def beautifulArray(self, n: int) -> list[int]:
+        if n == 1:
+            return [1]
+
+        odds = self.beautifulArray((n + 1) // 2)
+        evens = self.beautifulArray(n // 2)
+
+        result = []
+
+        for x in odds:
+            result.append(x * 2 - 1)
+
+        for x in evens:
+            result.append(x * 2)
+
+        return result
+
+
+s = Solution()
+print(s.beautifulArray(7))
