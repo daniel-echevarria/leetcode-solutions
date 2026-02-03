da4027ecfc4ad5f11e8a2dc6533fb64d743749cb
Kasper Bak <dan.echevarri@gmail.com>
2026-02-03 09:12:56 +0100

    Add solution

--- python/build-tree.py
+++ python/build-tree.py
@@ -16,7 +16,7 @@ class Solution:
             root_val = postorder.pop()
             root = TreeNode(root_val)
             root.right = helper(ino[ino.index(root_val) + 1 :])
-            root.left = helper(ino[0 : ino.index(root_val)])
+            root.left = helper(ino[: ino.index(root_val)])
             return root
 
         return helper(inorder)
@@ -24,13 +24,5 @@ class Solution:
 
 inorder = [9, 3, 15, 20, 7]
 postorder = [9, 15, 7, 20, 3]
-# inorder = [2, 1]
-# postorder = [2, 1]
-# inorder = [1, 2, 3, 4]
-# postorder = [2, 1, 4, 3]
 s = Solution()
 print(s.buildTree(inorder, postorder))
-# Algo, pop last value of postorder,
-# create a node, the right children are the right part of the numbers in the inorder
-# the left children are the left part
-# recurse
