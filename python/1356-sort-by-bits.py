ec416ebf448b8c8d50f3e2cd93d8fc6f2ccc8b7a
Kasper Bak <dan.echevarri@gmail.com>
2026-02-27 11:34:33 +0100

    first attempt

--- python/1356-sort-by-bits.py
+++ python/1356-sort-by-bits.py
@@ -1,7 +1,16 @@
 import math
+
+
 class Solution:
+    def sortBB(self, num):
+        return (bin(num)[2:]).count("1")
+
     def sortByBits(self, arr: list[int]) -> list[int]:
-        powers_of_two = []
-        rest = []
-        for n in arr:
-            if
+        arr.sort(key=self.sortBB)
+        return arr
+        print(arr)
+
+
+s = Solution()
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+print(s.sortByBits(arr))
