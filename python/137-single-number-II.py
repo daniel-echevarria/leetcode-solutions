13fd395742364699376e426d0dffe73e3492920d
Kasper Bak <dan.echevarri@gmail.com>
2026-03-02 11:19:29 +0100

    Add optimal solution (with help)

--- python/137-single-number-II.py
+++ python/137-single-number-II.py
@@ -1,13 +1,8 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        print("cool")
-
-
-n = 13
-counts = 0
-
-while n:
-    n = n & n - 1
-    counts += 1
-
-print(counts)
+    def singleNumber(self, nums: list[int]) -> int:
+        ones = 0
+        twos = 0
+        for n in nums:
+            ones = (ones ^ n) & ~twos
+            twos = (twos ^ n) & ~ones
+        return ones
