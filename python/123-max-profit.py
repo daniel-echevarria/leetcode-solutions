1ddcb652abfccb56b3bdb691ff703a5b89d24ed7
Kasper Bak <dan.echevarri@gmail.com>
2026-03-03 18:08:53 +0100

    Add optimal solution

--- python/123-max-profit.py
+++ python/123-max-profit.py
@@ -50,6 +50,22 @@ class Solution:
         return dp(0)
 
 
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        buy1 = float("-inf")
+        sell1 = float("-inf")
+        buy2 = float("-inf")
+        sell2 = float("-inf")
+
+        for p in prices:
+            buy1 = max(buy1, -p)
+            sell1 = max(sell1, buy1 + p)
+            buy2 = max(buy2, sell1 - p)
+            sell2 = max(sell2, buy2 + p)
+
+        return sell2
+
+
 s = Solution()
 prices = [3, 3, 5, 0, 0, 3, 1, 4]
 print(s.maxProfit(prices))
