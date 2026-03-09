3857d76199f103e768bdfbf3f5e6a3bc93ad1e40
Kasper Bak <dan.echevarri@gmail.com>
2026-03-09 14:34:40 +0100

    add problem and BF

--- python/443-string-compression.py
+++ python/443-string-compression.py
@@ -50,9 +50,61 @@ class Solution:
         return len(chars)
 
 
+class Solution:
+    def compress(self, chars: list[str]) -> int:
+        n = len(chars)
+        count = 0
+        l = r = current = 0
+        while r < n:
+            if chars[current] == chars[r]:
+                count += 1
+                r += 1
+                continue
+            if count > 1:
+                for c in str(count):
+                    chars[l + 1] = c
+                    l += 1
+
+            current = r
+            count = 0
+        if count > 1:
+            for c in str(count):
+                chars[l + 1] = c
+                l += 1
+        l += 1
+        return l + 1
+
+
+class Solution:
+    def compress(self, chars: list[str]) -> int:
+        current = chars[0]
+        count = 1
+        w, r = -1, 1
+        while r < len(chars):
+            if chars[r] == current:
+                count += 1
+                r += 1
+                continue
+            w += 1
+            chars[w] = current
+            if count > 1:
+                for n in str(count):
+                    w += 1
+                    chars[w] = n
+            current = chars[r]
+            count = 0
+        w += 1
+        chars[w] = current
+        if count > 1:
+            for n in str(count):
+                w += 1
+                chars[w] = n
+        return w
+
+
 s = Solution()
-# chars = ["a", "a", "b", "b", "c", "c", "c"]
 chars = ["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]
+# chars = ["a", "a", "a", "b", "b", "a", "a"]
 print(s.compress(chars))
 
 # Algo
