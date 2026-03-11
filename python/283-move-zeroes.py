129d81ba54528a8db859415a36e125784f8b9b10
Kasper Bak <dan.echevarri@gmail.com>
2026-03-11 10:50:24 +0100

    add optimal solution

--- python/283-move-zeroes.py
+++ python/283-move-zeroes.py
@@ -23,10 +23,29 @@ class Solution:
                 r += 1
 
 
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        n = len(nums)
+        i = 0
+        while i < n and nums[i] != 0:
+            i += 1
+        for j in range(i, n):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
+
+# class Solution:
+#     def moveZeroes(self, nums: list[int]) -> None:
+#         write = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0
+
+
 s = Solution()
-# nums = [0, 1, 0, 3, 12]
+nums = [5, 3, 1, 4, 6, 7, 0, 3, 12]
 # nums = [1, 0, 1]
-nums = [2, 1]
+# nums = [2, 1]
 print(s.moveZeroes(nums))
 
 # use two pointers
