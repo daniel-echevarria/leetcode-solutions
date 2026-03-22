fe4a6f3a5c98db4212371b56bf140970cce53078
Kasper Bak <dan.echevarri@gmail.com>
2026-03-22 14:26:56 +0100

    Add problem with solution

--- python/328-odd-even-linked-list.py
+++ python/328-odd-even-linked-list.py
@@ -18,3 +18,51 @@ class Solution:
 
         prev.next = even_ref.next
         return head
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next or not head.next.next:
+            return head
+
+        odd = head
+        even = head.next
+        og_even = even
+        prev = None
+        while odd and even:
+            if odd.next:
+                prev = odd
+                odd.next = odd.next.next
+                odd = odd.next
+            if even.next:
+                even.next = even.next.next
+                even = even.next
+        if odd:
+            odd.next = og_even
+        else:
+            prev.next = og_even
+        return head
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        odd = head
+        even = head.next
+        head_even = even
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+
+        odd.next = head_even
+        return head
