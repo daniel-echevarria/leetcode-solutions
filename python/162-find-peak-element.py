054481db7aada0c404fa7675b297968ada6a3447
Kasper Bak <dan.echevarri@gmail.com>
2026-04-08 18:03:28 +0200

    add fresh solution

--- python/162-find-peak-element.py
+++ python/162-find-peak-element.py
@@ -10,3 +10,28 @@ class Solution:
                 return peak_finder(mid + 1, r)
 
         return peak_finder(0, len(nums) - 1)
+
+
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return 0
+        l, r = 0, n - 1
+        while l < r:
+            mid = (l + r) // 2
+            if mid == 0:
+                return 0 if nums[0] > nums[1] else 1
+
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            elif nums[mid] < nums[mid - 1]:
+                r = mid - 1
+            else:
+                return mid
+        return l
+
+
+nums = [1, 2, 1, 3, 5, 6, 4]
+s = Solution()
+print(s.findPeakElement(nums))
