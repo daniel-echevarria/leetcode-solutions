ad535658e34c4084b1b04684abc949d45165d9a1
Kasper Bak <dan.echevarri@gmail.com>
2026-04-09 14:27:01 +0200

    add optimal solution

--- python/162-find-peak-element.py
+++ python/162-find-peak-element.py
@@ -32,6 +32,19 @@ class Solution:
         return l
 
 
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l
+
+
 nums = [1, 2, 1, 3, 5, 6, 4]
 s = Solution()
 print(s.findPeakElement(nums))
