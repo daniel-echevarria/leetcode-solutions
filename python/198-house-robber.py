8b4c876757778f338091441537df130fb963fcb2
Kasper Bak <dan.echevarri@gmail.com>
2026-04-12 14:39:15 +0200

    rediscover soution

--- python/198-house-robber.py
+++ python/198-house-robber.py
@@ -8,10 +8,28 @@ class Solution:
         return curr
 
 
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return max(nums)
+        if n < 4:
+            return max(nums[1], nums[0] + nums[2])
+        maxMoney = [0] * n
+        maxMoney[0] = nums[0]
+        maxMoney[1] = nums[1]
+        maxMoney[2] = nums[2] + nums[0]
+        for i in range(3, n):
+            maxMoney[i] = max(maxMoney[i - 2], maxMoney[i - 3]) + nums[i]
+        return max(maxMoney)
+
+
 s = Solution()
 # nums = [1, 2, 3, 1]
 # nums = [9, 1, 1, 9]
 # nums = [2, 7, 9, 3, 1]
-nums = [2, 1, 1, 2]
+# nums = [2, 1, 1, 2]
+nums = [1, 7, 9, 2]
+
 
 print(s.rob(nums))
