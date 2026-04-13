fdf8b3bb76251378c40637d19f27775306314fda
Kasper Bak <dan.echevarri@gmail.com>
2026-04-13 14:29:40 +0200

    add problem and bang head against wall

---
+++ python/714-max-profit.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxProfit(self, prices: list[int], fee: int) -> int:
+        n = len(prices)
+        max_profit = 0
+
+        def helper(idx, holding, profit):
+            nonlocal max_profit
+            if idx == n:
+                max_profit = max(profit, max_profit)
+                return
+            if holding:
+                helper(idx + 1, False, profit + prices[idx] - fee)
+                helper(idx + 1, True, profit)
+            else:
+                helper(idx + 1, True, profit - prices[idx])
+                helper(idx + 1, False, profit)
+
+        helper(0, False, 0)
+        return max_profit
+
+
+class Solution:
+    def maxProfit(self, prices: list[int], fee: int) -> int:
+        n = len(prices)
+        dp = [0] * n + 1
+        for i in range(1, len(dp)):
+            dp[i] = max
+
+
+# prices = [1, 3, 2, 8, 4, 9]
+# fee = 2
+prices = [1, 3, 7, 5, 10, 3]
+fee = 3
+
+s = Solution()
+print(s.maxProfit(prices, fee))
