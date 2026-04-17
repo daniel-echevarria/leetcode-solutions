2a796abdc420b7466760ddf8570a933d10933ea5
Kasper Bak <dan.echevarri@gmail.com>
2026-04-17 11:54:16 +0200

    add new solution

--- python/136-single-number.py
+++ python/136-single-number.py
@@ -6,6 +6,15 @@ class Solution:
         return start
 
 
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+
+        res = 0
+        for n in nums:
+            res ^= n
+        return res
+
+
 s = Solution()
 nums = [2, 2, 1]
 print(s.singleNumber(nums))
