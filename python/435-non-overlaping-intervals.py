64beff4dc136d03007360ea0d6f1abc899edd40a
Kasper Bak <dan.echevarri@gmail.com>
2026-04-21 11:21:26 +0200

    add problem with optimal solution (memorized)

--- python/435-non-overlaping-intervals.py
+++ python/435-non-overlaping-intervals.py
@@ -1,2 +1,23 @@
 class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+    def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        removals = 0
+        prev_end = float("-inf")
+
+        for start, end in intervals:
+            if start < prev_end:
+                removals += 1
+                continue
+            prev_end = end
+
+        return removals
+
+
+intervals = [[1, 2], [2, 3], [3, 4], [1, 3]]
+s = Solution()
+print(s.eraseOverlapIntervals(intervals))
+
+
+# Sort intervals by finish time
+# then iterate and when they overlap remove the one finishing later
+# count removals
