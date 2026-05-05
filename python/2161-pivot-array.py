7049073f5618d3ef25701aa22622a99ddff3ea01
Kasper Bak <dan.echevarri@gmail.com>
2026-05-05 11:44:36 +0200

    Add solution

--- python/2161-pivot-array.py
+++ python/2161-pivot-array.py
@@ -1,2 +1,20 @@
 class Solution:
-    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+    def pivotArray(self, nums: list[int], pivot: int) -> list[int]:
+        prefix = []
+        equal = []
+        suffix = []
+        for n in nums:
+            if n < pivot:
+                prefix.append(n)
+            elif n > pivot:
+                suffix.append(n)
+            else:
+                equal.append(n)
+        return prefix + equal + suffix
+
+
+nums = [9, 12, 5, 10, 14, 3, 10]
+pivot = 10
+
+s = Solution()
+print(s.pivotArray(nums, pivot))
