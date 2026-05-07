7cf3c18542758e9f73251fcb5c41cf3e21b1216e
Kasper Bak <dan.echevarri@gmail.com>
2026-05-07 10:58:50 +0200

    Add suboptimal solution

--- python/807-max-increase.py
+++ python/807-max-increase.py
@@ -1,2 +1,23 @@
 class Solution:
-    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+    def maxIncreaseKeepingSkyline(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        col_grid = []
+        total_height = 0
+
+        for i in range(n):
+            col = []
+            for j in range(n):
+                col.append(grid[j][i])
+            col_grid.append(col)
+
+        for i in range(n):
+            for j in range(n):
+                new_height = min(max(grid[i]), max(col_grid[j]))
+                previous_height = grid[i][j]
+                total_height += new_height - previous_height
+        return total_height
+
+
+grid = [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]
+s = Solution()
+print(s.maxIncreaseKeepingSkyline(grid))
