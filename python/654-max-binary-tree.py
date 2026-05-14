1f6c41e6c26d144ecda22eb877896685c18aa157
Kasper Bak <dan.echevarri@gmail.com>
2026-05-14 14:26:34 +0200

    Add solution

--- python/654-max-binary-tree.py
+++ python/654-max-binary-tree.py
@@ -1,8 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+    def constructMaximumBinaryTree(self, nums: list[int]):
+        if not nums:
+            return
+        val = max(nums)
+        idx = nums.index(val)
+
+        node = TreeNode(val)
+        node.left = self.constructMaximumBinaryTree(nums[:idx])
+        node.right = self.constructMaximumBinaryTree(nums[idx + 1 :])
+        return node
+
+
+nums = [3, 2, 1, 6, 0, 5]
+s = Solution()
+print(s.constructMaximumBinaryTree(nums))
+
+
+# Algo make an array including the index as the second number
+# recursively construct the maxBinaryTree
