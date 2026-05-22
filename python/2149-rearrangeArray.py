841637013c4901383d48c515bb1fa6a69f404b02
Kasper Bak <dan.echevarri@gmail.com>
2026-05-22 11:50:15 +0200

    Add 2 pointer solution

--- python/2149-rearrangeArray.py
+++ python/2149-rearrangeArray.py
@@ -13,5 +13,29 @@ class Solution:
             res.append(neg[i])
         return res
 
+
 class Solution:
     def rearrangeArray(self, nums: list[int]) -> list[int]:
+        pos = 0
+        neg = 0
+        res = []
+        next_is_pos = 1
+        while len(res) < len(nums):
+            if next_is_pos:
+                while nums[pos] < 0:
+                    pos += 1
+                res.append(nums[pos])
+                pos += 1
+                next_is_pos = 0
+            else:
+                while nums[neg] > 0:
+                    neg += 1
+                res.append(nums[neg])
+                neg += 1
+                next_is_pos = 1
+        return res
+
+
+nums = [3, 1, -2, -5, 2, -4]
+s = Solution()
+print(s.rearrangeArray(nums))
