eb5458f1cffbba5f613aaf442bfd17a3da98d5a2
Kasper Bak <dan.echevarri@gmail.com>
2026-06-06 14:07:09 +0200

    feat: add problem with solution

---
+++ python/1877-min-pair-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minPairSum(self, nums: list[int]) -> int:
+        nums.sort()
+        max_pair = float("-inf")
+        l, r = 0, len(nums) - 1
+        while l < r:
+            max_pair = max(max_pair, nums[l] + nums[r])
+            l += 1
+            r -= 1
+
+        return max_pair
