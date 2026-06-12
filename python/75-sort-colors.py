092392595eb4cbeaafdfd68c5c06460f7b765b55
Kasper Bak <dan.echevarri@gmail.com>
2026-06-12 12:25:36 +0200

    feat: add solution

--- python/75-sort-colors.py
+++ python/75-sort-colors.py
@@ -1,5 +1,24 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n = len(nums)
+        l, i, r = 0, 0, n - 1
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
+        return nums
+
+
+nums = [2, 0, 2, 1, 1, 0]
+# nums = [2, 0, 1]
+s = Solution()
+print(s.sortColors(nums))
