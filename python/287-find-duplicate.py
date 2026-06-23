0723f54a3646afe50f940cc020c5826ab3147fdb
Kasper Bak <dan.echevarri@gmail.com>
2026-06-23 13:30:31 +0200

    add problem and brute force

--- python/287-find-duplicate.py
+++ python/287-find-duplicate.py
@@ -1,2 +1,7 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(1, n):
+            counts = nums.count(i)
+            if counts > 1:
+                return i
