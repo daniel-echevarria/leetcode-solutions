397ce318ad577284d3447ae72896376891045ae5
Kasper Bak <dan.echevarri@gmail.com>
2026-06-24 12:04:33 +0200

    add hare and turtoise

--- python/287-find-duplicate.py
+++ python/287-find-duplicate.py
@@ -5,3 +5,19 @@ class Solution:
             counts = nums.count(i)
             if counts > 1:
                 return i
+
+
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        fast = slow = 0
+        n = len(nums)
+        nodes_visited = 0
+
+        while nodes_visited < n:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if nums[slow] == nums[fast]:
+                return nums[slow]
+            nodes_visited += 1
+
+        return False
