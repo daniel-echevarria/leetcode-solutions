ccfd92d8a09ea170e2ac3da286bc6aad98b520ba
Kasper Bak <dan.echevarri@gmail.com>
2026-07-04 16:05:03 +0200

    Add solution

--- python/287-find-duplicate.py
+++ python/287-find-duplicate.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate(self, nums: list[int]) -> int:
         n = len(nums)
         for i in range(1, n):
             counts = nums.count(i)
@@ -10,14 +10,24 @@ class Solution:
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
         fast = slow = 0
-        n = len(nums)
-        nodes_visited = 0
 
-        while nodes_visited < n:
+        while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            if nums[slow] == nums[fast]:
-                return nums[slow]
-            nodes_visited += 1
+            if slow == fast:
+                break
 
-        return False
+        slow2 = 0
+
+        while fast != slow2:
+            fast = nums[fast]
+            slow2 = nums[slow2]
+
+        return slow2
+
+
+nums = [2, 5, 9, 6, 9, 3, 8, 9, 7, 1]
+# nums = [3, 1, 3, 4, 2]
+
+s = Solution()
+print(s.findDuplicate(nums))
