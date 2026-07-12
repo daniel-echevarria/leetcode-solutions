09b035b092d8a1ca4e303189d7e92b4ca169d408
Kasper Bak <dan.echevarri@gmail.com>
2026-07-12 14:31:07 +0200

    add optimal solution

--- python/475-heaters.py
+++ python/475-heaters.py
@@ -1,2 +1,31 @@
+import bisect
+
+
 class Solution:
-    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+    def findRadius(self, houses: list[int], heaters: list[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        res = 0
+        n = len(heaters)
+
+        for h in houses:
+            idx = bisect.bisect(heaters, h)
+            if idx == 0:
+                res = max(res, heaters[0] - h)
+            elif idx == n:
+                res = max(res, h - heaters[n - 1])
+            else:
+                res = max(res, min(h - heaters[idx - 1], heaters[idx] - h))
+        return res
+
+
+# houses = [1, 2, 3]
+# heaters = [2]
+# houses = [1, 5]
+# heaters = [2]
+
+houses = [1, 2, 3, 4]
+heaters = [1, 4]
+
+s = Solution()
+print(s.findRadius(houses, heaters))
