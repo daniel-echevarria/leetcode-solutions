f6806bc538b44afb6d8275c90f151e240fe16429
Kasper Bak <dan.echevarri@gmail.com>
2026-08-05 14:07:12 +0200

    feat: add optimal solution (with help)

--- python/795-num-subarrays.py
+++ python/795-num-subarrays.py
@@ -1,2 +1,24 @@
 class Solution:
-    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+    def numSubarrayBoundedMax(self, nums: list[int], left: int, right: int) -> int:
+        def count(maxVal):
+            curr = 0
+            res = 0
+
+            for val in nums:
+                if val > maxVal:
+                    curr = 0
+                else:
+                    curr += 1
+                res += curr
+
+            return res
+
+        return count(right) - count(left - 1)
+
+
+nums = [2, 9, 2, 5, 6]
+left = 2
+right = 8
+
+s = Solution()
+print(s.numSubarrayBoundedMax(nums, left, right))
