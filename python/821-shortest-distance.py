f18eff5eb08f841d30fcc04ed58225c7b81c14e5
Kasper Bak <dan.echevarri@gmail.com>
2026-08-13 12:22:48 +0200

    feat: add solution

--- python/821-shortest-distance.py
+++ python/821-shortest-distance.py
@@ -22,6 +22,24 @@ class Solution:
         return res
 
 
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> list[int]:
+        n = len(s)
+        res = [n] * n
+
+        prev = float("-inf")
+        for i in range(n):
+            if s[i] == c:
+                prev = i
+            res[i] = i - prev
+        prev = float("inf")
+        for i in range(n - 1, -1, -1):
+            if s[i] == c:
+                prev = i
+            res[i] = min(res[i], prev - i)
+        return res
+
+
 st = "loveleetcode"
 c = "e"
 s = Solution()
