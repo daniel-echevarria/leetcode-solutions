3f629f62793ce8158654924830d978617089975d
Kasper Bak <dan.echevarri@gmail.com>
2026-08-13 12:23:15 +0200

    feat: add solution

--- python/832-flipping-an-image.py
+++ python/832-flipping-an-image.py
@@ -1,2 +1,29 @@
 class Solution:
-    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+    def flipAndInvertImage(self, image: list[list[int]]) -> list[list[int]]:
+
+        def invert(val):
+            return 1 if val == 0 else 0
+
+        n = len(image)
+        for row in image:
+            j, k = 0, n - 1
+            while j < k:
+                row[j], row[k] = row[k], row[j]
+                row[j] = invert(row[j])
+                row[k] = invert(row[k])
+                j += 1
+                k -= 1
+        return image
+
+
+class Solution:
+    def flipAndInvertImage(self, image: list[list[int]]) -> list[list[int]]:
+        def invert(val):
+            return 1 if val == 0 else 0
+
+        return [[invert(x) for x in reversed(row)] for row in image]
+
+
+image = [[1, 1, 0], [1, 0, 1], [0, 0, 0]]
+s = Solution()
+print(s.flipAndInvertImage(image))
