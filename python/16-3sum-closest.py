c886c276c6b158b91b3da5369c9d9153783d30ef
Kasper Bak <dan.echevarri@gmail.com>
2026-08-17 09:23:43 +0200

    feat: add new solution

--- python/16-3sum-closest.py
+++ python/16-3sum-closest.py
@@ -24,6 +24,35 @@ class Solution:
         return res
 
 
+class Solution:
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        res = None
+        closest = float("inf")
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l, r = i + 1, n - 1
+
+            while l < r:
+                curr_sum = nums[i] + nums[l] + nums[r]
+                diff = abs(curr_sum - target)
+                if diff < closest:
+                    closest = diff
+                    res = curr_sum
+
+                if curr_sum == target:
+                    return curr_sum
+                elif curr_sum < target:
+                    l += 1
+                else:
+                    r -= 1
+        return res
+
+
 nums = [-1, 2, 1, -4]
 target = 1
 
