08c906d6cf375620052fb684a2eeaa40fb3162ee
Kasper Bak <dan.echevarri@gmail.com>
2026-08-17 09:23:57 +0200

    feat: add new solution

--- python/18-foursum.py
+++ python/18-foursum.py
@@ -30,6 +30,40 @@ class Solution:
         return quadruplets
 
 
+class Solution:
+    def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                l, r = j + 1, n - 1
+
+                while l < r:
+                    curr_sum = nums[i] + nums[j] + nums[l] + nums[r]
+                    if curr_sum == target:
+                        res.append((nums[i], nums[j], nums[l], nums[r]))
+                        l += 1
+                        r -= 1
+
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r + 1]:
+                            r -= 1
+                    elif curr_sum < target:
+                        l += 1
+                    else:
+                        r -= 1
+        return res
+
+
 # nums = [1, 0, -1, 0, -2, 2]
 # target = 0
 nums = [2, 2, 2, 2, 2]
