d3d5e4cb3fe46fbfa6208e8895a90616fe634d73
Kasper Bak <dan.echevarri@gmail.com>
2026-08-17 10:08:37 +0200

    feat: add new solution

--- python/11-container-with-most-water.py
+++ python/11-container-with-most-water.py
@@ -40,6 +40,37 @@ class Solution:
         return res
 
 
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        n = len(height)
+        l, r = 0, n - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+
+
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+
+        l, r = 0, len(height) - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return res
+
+
 s = Solution()
 h = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 print(s.maxArea(h))
