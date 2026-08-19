22e85d7c49b2b1d4ff465f943dd531eb90fb93d7
Kasper Bak <dan.echevarri@gmail.com>
2026-08-19 15:19:32 +0200

    feat: add solution

--- python/27-remove-el.py
+++ python/27-remove-el.py
@@ -32,6 +32,19 @@ class Solution:
         return l
 
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] == val:
+                nums[l] = nums[r]
+                r -= 1
+            else:
+                l += 1
+        return l
+
+
 nums = [0, 1, 2, 2, 3, 0, 4, 2]
 val = 2
 s = Solution()
