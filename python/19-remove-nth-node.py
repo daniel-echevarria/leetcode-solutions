34191f542fed0b8b182e6e437bb774c62c509c56
Kasper Bak <dan.echevarri@gmail.com>
2026-08-19 15:19:16 +0200

    feat: practice solution

--- python/19-remove-nth-node.py
+++ python/19-remove-nth-node.py
@@ -59,3 +59,24 @@ class Solution:
         slow.next = slow.next.next
 
         return dummy.next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        fast = slow = dummy
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        return dummy.next
+
+
+head = 0, [1, 2, 3, 4, 5]
+n = 2
+s = Solution()
