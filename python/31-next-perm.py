379abcb9f2b80f730a7cc7b4996290206b9f243d
Kasper Bak <dan.echevarri@gmail.com>
2026-08-19 15:19:47 +0200

    feat: add solution

--- python/31-next-perm.py
+++ python/31-next-perm.py
@@ -42,8 +42,37 @@ class Solution:
         nums.reverse()
 
 
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        switch_point: int = -1
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                switch_point = i
+                break
+
+        if switch_point == -1:
+            nums.sort()
+            return
+
+        for i in range(n - 1, -1, -1):
+            if nums[i] > nums[switch_point]:
+                nums[i], nums[switch_point] = nums[switch_point], nums[i]
+                break
+
+        nums[switch_point + 1 :] = reversed(nums[switch_point + 1 :])
+        return nums
+
+
+# Algo, find the first decreasing item from the end and mark it's index, if no such item is found, just sort the array
+# then iterate gain from the end and find the first element which is bigger than the
+# switch point element, switch them
+# then order the rest of the array
+
 # nums = [1, 3, 2, 5, 8, 6, 4]
 nums = [1, 3, 2]
 s = Solution()
 print(s.nextPermutation(nums))
-print(nums)
