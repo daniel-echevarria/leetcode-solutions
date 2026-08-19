896134c0277072ab27a2a5b7f365872f53fe968c
Kasper Bak <dan.echevarri@gmail.com>
2026-08-19 15:19:27 +0200

    feat: add solution

--- python/26-remove-duplicates.py
+++ python/26-remove-duplicates.py
@@ -54,6 +54,20 @@ class Solution:
         return write
 
 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        i = 1
+
+        for j in range(1, len(nums)):
+            if nums[j] == nums[j - 1]:
+                continue
+            nums[i] = nums[j]
+            i += 1
+
+        return i
+
+
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 # nums = [1, 1]
 s = Solution()
