3f4f1750ff5eb0bf7b9daebdca36aa98cfe18f5d
Kasper Bak <dan.echevarri@gmail.com>
2026-08-23 22:20:11 +0200

    feat: add solution

--- python/5-longest-palindrome.py
+++ python/5-longest-palindrome.py
@@ -125,6 +125,26 @@ class Solution:
         return res
 
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        longest = ""
+
+        def expand(i, j):
+            nonlocal longest
+            while i >= 0 and j < n and s[i] == s[j]:
+                if j - i + 1 > len(longest):
+                    longest = s[i : j + 1]
+                i -= 1
+                j += 1
+
+        for i in range(n):
+            expand(i, i)
+            expand(i, i + 1)
+
+        return longest
+
+
 st = "babad"
 s = Solution()
 print(s.longestPalindrome(st))
