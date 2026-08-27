2409dad3e2d63361f4cf54e2ffbbd2f23353782c
Kasper Bak <dan.echevarri@gmail.com>
2026-08-27 09:03:41 +0200

    feat: add new solution

--- python/82-remove-duplicates.py
+++ python/82-remove-duplicates.py
@@ -30,4 +30,20 @@ class Solution:
         return dummy.next
 
 
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                prev.next = head.next
+            else:
+                prev = prev.next
+            head = head.next
+        return dummy.next
+
+
 head = [1, 2, 3, 3, 4, 4, 5]
