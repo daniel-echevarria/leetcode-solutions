5556db72d225c89d4b96ed11cf1017bbcf55bd74
Kasper Bak <dan.echevarri@gmail.com>
2026-08-28 21:38:26 +0200

    feat: add solution

--- python/141-linked-list-cycle.py
+++ python/141-linked-list-cycle.py
@@ -17,6 +17,23 @@ class Solution:
         return False
 
 
-# Algo:
-# Iterate through the list while keeping track of the visited nodes in a set
-# if the next node is in the set return true else after traversin the list return false
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
