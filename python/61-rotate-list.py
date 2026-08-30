18dd8d57e8cf685a8f83637adabfab50407ee416
Kasper Bak <dan.echevarri@gmail.com>
2026-08-30 08:49:26 +0200

    feat: add solution

--- python/61-rotate-list.py
+++ python/61-rotate-list.py
@@ -116,4 +116,38 @@ class Solution:
         return new_head
 
 
-[1, 2, 3, 4, 5]
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or not k:
+            return head
+
+        length = 1
+        tail = head
+
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k %= length
+
+        if not k:
+            return head
+
+        new_tail = head
+        for _ in range(length - k - 1):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
+        tail.next = head
+
+        return new_head
+
+
+# [1, 2, 3, 4, 5]
+[0, 1, 2]
