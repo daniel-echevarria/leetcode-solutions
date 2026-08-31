70b023ce62e34ac7d1caf021c9d9bd3d85637ae6
Kasper Bak <dan.echevarri@gmail.com>
2026-08-31 13:21:20 +0200

    feat: add new solution

--- python/80-remove-duplicates.py
+++ python/80-remove-duplicates.py
@@ -65,6 +65,41 @@ class Solution:
         return write
 
 
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        n = len(nums)
+        write = read = 2
+        while read < n:
+            while write < n - 1 and nums[write] != nums[write - 2]:
+                write += 1
+            while read < n - 1 and nums[read] <= nums[write]:
+                read += 1
+            nums[write] = nums[read]
+            write += 1
+            read += 1
+        return write
+
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        write = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[write - 2]:
+                nums[write] = nums[i]
+                write += 1
+        return write
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        write = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[write - 2]:
+                nums[write] = nums[i]
+                write += 1
+        return write
+
+
 # nums = [1, 1, 1, 2, 2, 3]
 nums = [0, 0, 1, 1, 1, 1, 2, 3, 3]
 
