2f3df23ddefa8c2bd763ca240cb090577af4eb0d
Daniel Echevarria <dan.echevarri@gmail.com>
2026-09-04 09:43:33 +0200

    feat: rederive solution

--- python/141-linked-list-cycle.py
+++ python/141-linked-list-cycle.py
@@ -7,33 +7,12 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hare = head
-        turtle = head
-        while hare and hare.next:
-            hare = hare.next.next
-            turtle = turtle.next
-            if hare == turtle:
-                return True
-        return False
-
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-
-        fast = slow = head
+        slow = fast = head
 
         while fast and fast.next:
-            fast = fast.next.next
             slow = slow.next
-            if fast == slow:
+            fast = fast.next.next
+            if slow == fast:
                 return True
+
         return False
