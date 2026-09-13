2c93ea8a02d3ae2b8b92cb32205e844b4f5849ca
Daniel Echevarria <dan.echevarri@gmail.com>
2026-09-13 14:31:10 +0200

    feat: re-derive solution

--- python/234-palindrome-ll-2.py
+++ python/234-palindrome-ll-2.py
@@ -8,12 +8,59 @@ class Solution:
         slow = fast = head
         prev = None
 
-        while fast.next.next:
+        while fast.next and fast.next.next:
             fast = fast.next.next
             temp = slow.next
             slow.next = prev
             prev = slow
             slow = temp
 
-        if fast.next:
+        if fast == slow:
+            return True if not fast.next else fast.val == fast.next.val
 
+        right = slow.next
+        slow.next = prev
+
+        if not fast.next:
+            slow = slow.next
+        left = slow
+
+        while left or right:
+            if not left or not right or left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+
+        slow = fast = head
+        prev = None
+
+        while fast and fast.next:
+            fast = fast.next.next
+
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        if fast:
+            slow = slow.next
+
+        left, right = prev, slow
+
+        while left and right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+
+
+[1, 2, 2, 1]
+[0, 0]
