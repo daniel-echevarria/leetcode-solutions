1cd3cdc8bc4dc90ee3a0852ea80c663ffcca5916
Daniel Echevarria <dan.echevarri@gmail.com>
2026-09-21 15:08:16 +0200

    re-derive solution

---
+++ python/455-assign-cookies-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findContentChildren(self, g: list[int], s: list[int]) -> int:
+        g.sort()
+        s.sort()
+
+        child = cookie = 0
+
+        while cookie < len(s) and child < len(g):
+            if s[cookie] >= g[child]:
+                child += 1
+            cookie += 1
+
+        return child
