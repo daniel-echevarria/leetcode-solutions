226ab009175d7e168f4409e1da073f864e1b4823
Daniel Echevarria <dan.echevarri@gmail.com>
2026-09-21 15:08:46 +0200

    feat: bang head

--- python/457-circular-array-loop.py
+++ python/457-circular-array-loop.py
@@ -25,6 +25,62 @@ class Solution:
         return False
 
 
-nums = [2, -1, 1, 2, 2]
+class Solution:
+    def circularArrayLoop(self, nums: list[int]) -> bool:
+        def move_next(idx):
+            return (nums[idx] + idx) % len(nums)
+
+        def same_direction(i, moving_forward):
+            return nums[i] > 0 == moving_forward
+
+        for i, n in enumerate(nums):
+            fast = slow = i
+            moving_forward = nums[i] > 0
+            while True:
+                prev = slow
+                slow = move_next(slow)
+                fast = move_next(move_next(fast))
+
+                if (
+                    nums[slow] > 0
+                    and not moving_forward
+                    or nums[slow] < 0
+                    and moving_forward
+                    or prev == slow
+                ):
+                    break
+
+                if slow == fast:
+                    return True
+        return False
+
+
+class Solution:
+    def circularArrayLoop(self, nums: list[int]) -> bool:
+        n = len(nums)
+
+        def next_index(i):
+            return (i + nums[i]) % n
+
+        for i in range(n):
+            slow = fast = i
+            direction = nums[i] > 0
+
+            while (
+                (nums[slow] > 0) == direction
+                and (nums[fast] > 0) == direction
+                and (nums[next_index(fast)] > 0) == direction
+            ):
+                slow = next_index(slow)
+                fast = next_index(next_index(fast))
+
+                if fast == slow:
+                    if next_index(slow) == slow:
+                        break
+                    return True
+        return False
+
+
+nums = [1, -1, 5, 1, 4]
 s = Solution()
 print(s.circularArrayLoop(nums))
